11ca5b7a2100ecde0dccf22827fa9337da1a4b4b
PikachuHy <pikachuhy@163.com>
2022-04-03 15:18:55 +0800

    fix: Undefined symbols

---
+++ src/Plugins/Qt/QTMApplication.cpp
@@ -0,0 +1,19 @@
+
+/******************************************************************************
+ * MODULE     : QTMApplication.hpp
+ * DESCRIPTION:
+ * COPYRIGHT  : (C) 2022 PikachuHy
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
+/*
+add this empty file to eliminate the following error
+
+Undefined symbols for architecture x86_64:
+  "vtable for QTMApplication", referenced from:
+      QTMApplication::QTMApplication(int&, char**) in texmacs.cpp.o
+  NOTE: a missing vtable usually means the first non-inline virtual member function has no definition.
+*/
---
+++ src/Plugins/Qt/QTMInteractiveInputHelper.cpp
@@ -0,0 +1,19 @@
+
+/******************************************************************************
+* MODULE     : QTMInteractiveInputHelper.hpp
+* DESCRIPTION: QT Interactive Input Helper class
+* COPYRIGHT  : (C) 2022 PikachuHy
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
+/*
+add this empty file to eliminate the following error
+
+Undefined symbols for architecture x86_64:
+  "vtable for QTMInteractiveInputHelper", referenced from:
+      QTMInteractiveInputHelper::QTMInteractiveInputHelper(qt_tm_widget_rep*) in qt_tm_widget.cpp.o
+  NOTE: a missing vtable usually means the first non-inline virtual member function has no definition.
+*/
