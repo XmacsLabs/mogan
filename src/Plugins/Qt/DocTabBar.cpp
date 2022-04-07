3c63cdb216c59fefb76984ccc15e4938ca36ad5f
PikachuHy <pikachuhy@163.com>
2022-04-07 19:24:54 +0800

    format

--- src/Plugins/Qt/DocTabBar.cpp
+++ src/Plugins/Qt/DocTabBar.cpp
@@ -1,13 +1,12 @@
 /******************************************************************************
-* MODULE     : texmacs.cpp
-* DESCRIPTION: Document Tab Bar
-* COPYRIGHT  : (C) 2022  PikachuHy
-*******************************************************************************
-* This software falls under the GNU general public license version 3 or later.
-* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
-* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
-******************************************************************************/
-
+ * MODULE     : texmacs.cpp
+ * DESCRIPTION: Document Tab Bar
+ * COPYRIGHT  : (C) 2022  PikachuHy
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
 
 #include "DocTabBar.h"
 #include "new_buffer.hpp"
@@ -26,7 +25,8 @@ DocTabBar::DocTabBar (QWidget *parent) : QTabBar (parent) {
   connect (this, &DocTabBar::tabCloseRequested, this,
            &DocTabBar::handleTabCloseRequested);
 }
-void DocTabBar::updateTabs (const url &cur_buffer) {
+void
+DocTabBar::updateTabs (const url &cur_buffer) {
   for (int i= 0; i < m_buffers.size (); ++i) {
     if (m_buffers[i] == cur_buffer) {
       setCurrentIndex (i);
@@ -38,10 +38,12 @@ void DocTabBar::updateTabs (const url &cur_buffer) {
   addTab (to_qstring (title));
   setCurrentIndex (m_buffers.size () - 1);
 }
-void DocTabBar::handleTabBarClicked (int index) {
+void
+DocTabBar::handleTabBarClicked (int index) {
   call ("switch-to-buffer*", object (m_buffers[index]));
 }
-void DocTabBar::handleTabCloseRequested (int index) {
+void
+DocTabBar::handleTabCloseRequested (int index) {
   kill_buffer (m_buffers[index]);
   m_buffers.removeAt (index);
   removeTab (index);
--- src/Plugins/Qt/DocTabBar.h
+++ src/Plugins/Qt/DocTabBar.h
@@ -1,12 +1,12 @@
 /******************************************************************************
-* MODULE     : texmacs.cpp
-* DESCRIPTION: Document Tab Bar
-* COPYRIGHT  : (C) 2022  PikachuHy
-*******************************************************************************
-* This software falls under the GNU general public license version 3 or later.
-* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
-* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
-******************************************************************************/
+ * MODULE     : texmacs.cpp
+ * DESCRIPTION: Document Tab Bar
+ * COPYRIGHT  : (C) 2022  PikachuHy
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
 
 #ifndef TEXMACS_DOCTABBAR_H
 #define TEXMACS_DOCTABBAR_H
