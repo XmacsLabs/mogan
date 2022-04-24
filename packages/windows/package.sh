47324131ff692ae707b463814123df2b89b79f20
沈达 <sadhen@zoho.com>
2022-04-24 22:35:18 +0800

    CMake: build and package for 32/64 bit

--- packages/windows/package.sh
+++ packages/windows/package.sh
@@ -3,6 +3,14 @@
 export PATH=$PATH:$HOME/github/mxe/mxe/usr/bin
 
 mkdir -p build && cd build/
-x86_64-w64-mingw32.static-cmake .. -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan
+if [ $1 = "win32" ];then
+  i686-w64-mingw32.static-cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan \
+    -DWIN_BIT_SIZE=32
+else
+  x86_64-w64-mingw32.static-cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan \
+    -DWIN_BIT_SIZE=64
+fi
 make -j12 && make install -j12
 
