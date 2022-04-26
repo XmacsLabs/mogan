1d7e2188d6d539c1462869cfef49f2efe674fa8f
沈达 <sadhen@zoho.com>
2022-04-26 20:11:49 +0800

    Fix

--- packages/windows/package.sh
+++ packages/windows/package.sh
@@ -3,7 +3,7 @@
 export PATH=$PATH:$HOME/github/mxe/mxe/usr/bin
 
 mkdir -p build && cd build/
-if [ $1 = "win32" ];then
+if [ "$1" = "win32" ];then
   i686-w64-mingw32.static-cmake .. \
     -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan \
     -DWIN_BIT_SIZE=32
@@ -12,5 +12,6 @@ else
     -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan \
     -DWIN_BIT_SIZE=64
 fi
+
 make -j12 && make install -j12
 
