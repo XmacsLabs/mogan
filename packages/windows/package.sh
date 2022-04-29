db913dae7b26a4cdfa73c430c64cf0548fcf9fa0
Darcy Shen <sadhen@zoho.com>
2022-04-29 23:15:46 +0800

    Minor improvement for the win package script (#190)

--- packages/windows/package.sh
+++ packages/windows/package.sh
@@ -5,11 +5,11 @@ export PATH=$PATH:$HOME/github/mxe/mxe/usr/bin
 mkdir -p build && cd build/
 if [ "$1" = "win32" ];then
   i686-w64-mingw32.static-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan \
+    -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan32 \
     -DWIN_BIT_SIZE=32
 else
   x86_64-w64-mingw32.static-cmake .. \
-    -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan \
+    -DCMAKE_INSTALL_PREFIX=$HOME/win/Mogan64 \
     -DWIN_BIT_SIZE=64
 fi
 
