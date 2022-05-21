1ebfeca44aed31ecb7b66165999e068517858fe0
沈达 <sadhen@zoho.com>
2022-05-21 10:05:45 +0800

    styles

--- tests/Base/base.cpp
+++ tests/Base/base.cpp
@@ -1,3 +1,14 @@
+
+/******************************************************************************
+* MODULE     : base.cpp
+* DESCRIPTION: Implementation of base.hpp for test purpose
+* COPYRIGHT  : (C) 2022  Darcy Shen
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
 #include <QtTest/QtTest>
 #include "base.hpp"
 
