05a864a279f21665e566b786e05565b274ca5896
沈浪熊猫儿 <shenda@ustc.edu>
2022-06-21 21:49:25 +0800

    Code: construct prog language parser by defined format

--- src/System/Language/prog_language.cpp
+++ src/System/Language/prog_language.cpp
@@ -292,7 +292,8 @@ prog_language (string s) {
   if (s == "fortran")
     return make (language, s, tm_new<fortran_language_rep> (s));
 
-  if (format_exists (s)) make (language, s, tm_new<prog_language_rep> (s));
+  if (format_exists (s))
+    return make (language, s, tm_new<prog_language_rep> (s));
 
   return make (language, s, tm_new<verb_language_rep> (s));
 }
