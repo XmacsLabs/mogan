0ec1fc106912eabd94dc09874d890678cf726e5c
沈浪熊猫儿 <shenda@ustc.edu>
2022-07-24 17:49:49 +0800

    [no ci] macOS packager: rename from TeXmacs to Xmacs

--- packages/macos/package.sh
+++ packages/macos/package.sh
@@ -3,6 +3,6 @@
 mkdir -p build/ && cd build/
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./Mogan.app/Contents/Resources ..
 make -j12 install
-mv Mogan.app/Contents/Resources/bin Mogan.app/Contents/Resources/share/TeXmacs/
-cp /Applications/TeXmacs.app/Contents/Resources/share/TeXmacs/bin/gs Mogan.app/Contents/Resources/share/TeXmacs/bin/
+mv Mogan.app/Contents/Resources/bin Mogan.app/Contents/Resources/share/Xmacs/
+cp /Applications/TeXmacs.app/Contents/Resources/share/TeXmacs/bin/gs Mogan.app/Contents/Resources/share/Xmacs/bin/
 macdeployqt Mogan.app -verbose=1 -dmg
