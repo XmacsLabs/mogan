329b374bdf0fb1f9ed32206436d62d884a400af9
沈浪熊猫儿 <shenda@ustc.edu>
2022-09-07 22:24:17 +0800

    macOS: packaging with eukleides

--- packages/macos/package.sh
+++ packages/macos/package.sh
@@ -5,4 +5,6 @@ cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./Mogan.app/Contents/Res
 make -j12 install
 mv Mogan.app/Contents/Resources/bin Mogan.app/Contents/Resources/share/Xmacs/
 cp /Applications/TeXmacs.app/Contents/Resources/share/TeXmacs/bin/gs Mogan.app/Contents/Resources/share/Xmacs/bin/
+mkdir -p Mogan.app/Contents/Resources/share/Xmacs/plugins/eukleides/bin
+cp /Applications/Mogan.app/Contents/Resources/share/Xmacs/plugins/eukleides/bin/eukleides Mogan.app/Contents/Resources/share/Xmacs/plugins/eukleides/bin
 macdeployqt Mogan.app -verbose=1 -dmg
