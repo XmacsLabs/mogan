7bb130994044808f6f20e47fcae639b3dc7c5c3f
沈浪熊猫儿 <shenda@ustc.edu>
2022-10-04 23:43:58 +0800

    Use the env font for Chinese

--- src/Typeset/Concat/concater.cpp
+++ src/Typeset/Concat/concater.cpp
@@ -103,7 +103,12 @@ concater_rep::flag_ok (string s, path ip, color col) {
   }
   else if (info == INFO_DETAILED || info == INFO_PAPER) {
     int sz= env->get_script_size (env->fn_size, env->index_level+2);
-    font gfn (tex_font ("ecrm", sz, (int) (env->magn*env->dpi)));
+    font gfn;
+    if (is_cjk_unified_ideographs (s)) {
+      gfn= env->fn;
+    } else {
+      gfn= tex_font ("ecrm", sz, (int) (env->magn*env->dpi));
+    }
     box textb= text_box (decorate (ip), 0, s, gfn, col);
     box flagb= flag_box (dip, textb, h, pencil (col, env->fn->wline), light);
     if (info == INFO_DETAILED) {
