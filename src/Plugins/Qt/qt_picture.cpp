913f9eb048481369abae8a6211c89f5a9138565f
Darcy Shen <sadhen@zoho.com>
2022-10-16 01:17:21 +0800

    UI: SVG icon for Windows (#348)
    * UI: SVG icon for Windows
    
    * Cleaning
    
    * Re-org
    
    * re-add png for math
    
    * Cleaning
    
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

--- src/Plugins/Qt/qt_picture.cpp
+++ src/Plugins/Qt/qt_picture.cpp
@@ -96,7 +96,11 @@ qt_load_icon (url file_name) {
   if (exists (png)) {
     return QIcon (as_pixmap (*xpm_image (file_name)));
   } else if (exists (svg)) {
-    return QIcon (to_qstring (as_string (svg)));
+#ifdef OS_MINGW
+    return QIcon (qt_load_svg_icon (svg));
+#else
+   return QIcon (to_qstring (as_string (svg)));
+#endif
   } else {
     return QIcon (as_pixmap (*xpm_image (file_name)));
   }
@@ -358,6 +362,35 @@ new_qt_load_xpm (url file_name) {
                  (int) floor (f * pm.height () + 0.5),
                  Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
   return qt_picture (pm, 0, 0);
+}
+
+QPixmap
+qt_load_svg_icon (url file_name) {
+  string sss;
+
+  string sizeX =basename (url_parent (url_parent(file_name)));
+  QSize size;
+  double scale= max (retina_scale, (double) retina_icons);
+  if (sizeX == "16x16") {
+    size= QSize(int(16*scale), int(16*scale));
+  } else if (sizeX == "20x20") {
+    size= QSize(int(20*scale), int(20*scale));
+  } else if (sizeX == "24x24") {
+    size= QSize(int(24*scale), int(24*scale));
+  } else {
+    FAILED("not a icon");
+  }
+
+  QSvgRenderer renderer (to_qstring(as_string (file_name)));
+  QImage *pm= NULL;
+  pm= new QImage (size.width(), size.height(), QImage::Format_ARGB32);
+  pm->fill (Qt::transparent);
+  QPainter painter (pm);
+  renderer.render (&painter);
+
+  QPixmap icon (pm->size ());
+  icon.convertFromImage (*pm);
+  return icon;
 }
 
 picture
