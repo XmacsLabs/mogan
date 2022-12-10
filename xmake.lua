ea1d797f612a30ede582248619d87b82ab318e79
jingkaimori <36156959+jingkaimori@users.noreply.github.com>
2022-12-10 14:54:25 +0800

    Add package ci for windows 2h (#421)
    This ci will be triggered by pushing tag. Cache qt environment to
    accelerate ci execution. Use windeploy twice to avoid `Qt5Test` appears
    in installer.
    
    Binary dependencies is stored in
    [XmacsLabs/mogan-dependencies](https://github.com/XmacsLabs/mogan-dependencies)
    
    # see also
    
    * #116 
    * #127 
    * #128 
    * #129

--- xmake.lua
+++ xmake.lua
@@ -409,11 +409,6 @@ target("mogan") do
     after_install(function(target)
         local install_dir = path.join(target:installdir(), "/bin/")
         os.cp(target:targetfile(), install_dir)
-        local bin_dir = path.directory(target:targetfile())
-        os.cp(
-            path.join(bin_dir,"**.dll"),
-            install_dir, {rootdir=bin_dir}
-        )
     end)
 end 
 
