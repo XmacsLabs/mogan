26cb5ac97535cb41b1eaed3b8974b8cdaee00ad0
沈浪熊猫儿 <shenda@ustc.edu>
2023-01-22 21:29:08 +0800

    wip 30m

--- src/System/Language/text_language.cpp
+++ src/System/Language/text_language.cpp
@@ -414,6 +414,138 @@ oriental_language_rep::hyphenate (
   right= s (after+1, N(s));
 }
 
+
+/******************************************************************************
+* Chinese typography
+******************************************************************************/
+
+struct chinese_language_rep: language_rep {
+  hashmap<string,bool> punct;
+  hashmap<string,bool> wide_punct;
+  chinese_language_rep (string lan_name);
+  text_property advance (tree t, int& pos);
+  array<int> get_hyphens (string s);
+  void hyphenate (string s, int after, string& left, string& right);
+};
+
+chinese_language_rep::chinese_language_rep (string lan_name):
+  language_rep (lan_name), punct (false)
+{
+  // punct is for symbols which should not be the line start
+  list<string> half_width = list(".", ",", ":", ";", "!", "?")
+  for (int i=0; i<N(half_width); i++) {
+    punct (half_width[i])= true;
+  }
+  punct ("<#3000>")= true;
+  punct ("<#3001>")= true;
+  punct ("<#3002>")= true;
+  punct ("<#3003>")= true;
+  punct ("<#3004>")= true;
+  punct ("<#3005>")= true;
+  punct ("<#3006>")= true;
+  punct ("<#3007>")= true;
+  punct ("<#3008>")= true;
+  punct ("<#3009>")= true;
+  punct ("<#300a>")= true;
+  punct ("<#300b>")= true;
+  punct ("<#300c>")= true;
+  punct ("<#300d>")= true;
+  punct ("<#300e>")= true;
+  punct ("<#300f>")= true;
+  punct ("<#300A>")= true;
+  punct ("<#300B>")= true;
+  punct ("<#300C>")= true;
+  punct ("<#300D>")= true;
+  punct ("<#300E>")= true;
+  punct ("<#300F>")= true;
+  punct ("<#ff01>")= true;
+  punct ("<#ff0c>")= true;
+  punct ("<#ff0e>")= true;
+  punct ("<#ff1a>")= true;
+  punct ("<#ff1b>")= true;
+  punct ("<#ff1f>")= true;
+  punct ("<#FF01>")= true;
+  punct ("<#FF0C>")= true;
+  punct ("<#FF0E>")= true;
+  punct ("<#FF1A>")= true;
+  punct ("<#FF1B>")= true;
+  punct ("<#FF1F>")= true;
+
+  //wide_punct ("<#3001>")= true;
+  //wide_punct ("<#ff01>")= true;
+  //wide_punct ("<#ff0c>")= true;
+  //wide_punct ("<#ff0e>")= true;
+  //wide_punct ("<#ff1a>")= true;
+  //wide_punct ("<#ff1b>")= true;
+  //wide_punct ("<#ff1f>")= true;
+  //wide_punct ("<#FF01>")= true;
+  //wide_punct ("<#FF0C>")= true;
+  //wide_punct ("<#FF0E>")= true;
+  //wide_punct ("<#FF1A>")= true;
+  //wide_punct ("<#FF1B>")= true;
+  //wide_punct ("<#FF1F>")= true;
+}
+
+text_property
+chinese_language_rep::advance (tree t, int& pos) {
+  string s= t->label;
+  if (pos >= N(s)) return &tp_normal_rep;
+
+  if (s[pos] == ' ') {
+    pos++;
+    return &tp_space_rep;
+  }
+
+  if (s[pos] == '<' && !test (s, pos, "<#")) {
+    while ((pos<N(s)) && (s[pos]!='>')) pos++;
+    if (pos<N(s)) pos++;
+    return &tp_normal_rep;
+  }
+
+  if (pos < N(s) && !test (s, pos, "<#")) {
+    while (pos < N(s) && s[pos] != ' ' && s[pos] != '<')
+      tm_char_forwards (s, pos);
+    return &tp_cjk_no_break_rep;
+  }
+
+  int start= pos;
+  tm_char_forwards (s, pos);
+  string c= s (start, pos);
+  int next= pos;
+  tm_char_forwards (s, next);
+  string x= s (pos, next);
+
+  if (punct->contains (c)) {
+    if (punct->contains (x) || pos == N(s))
+      return &tp_cjk_no_break_period_rep;
+    else if (wide_punct->contains (c))
+      return &tp_cjk_wide_period_rep;
+    else return &tp_cjk_period_rep;
+  }
+  else {
+    if (punct->contains (x) || pos == N(s))
+      return &tp_cjk_no_break_rep;
+    else return &tp_cjk_normal_rep;
+  }
+}
+
+array<int>
+chinese_language_rep::get_hyphens (string s) {
+  int i;
+  array<int> penalty (N(s)+1);
+  for (i=0; i<N(penalty); i++) penalty[i]= HYPH_INVALID;
+  return penalty;
+}
+
+void
+chinese_language_rep::hyphenate (
+  string s, int after, string& left, string& right)
+{
+  left = s (0, after+1);
+  right= s (after+1, N(s));
+}
+
+
 /******************************************************************************
 * Main interface
 ******************************************************************************/
@@ -474,13 +606,18 @@ make_oriental_language (string s) {
   return tm_new<oriental_language_rep> (s);
 }
 
+static language
+make_chinese_language (string s) {
+  return tm_new<chinese_language_rep> (s);
+}
+
 language
 text_language (string s) {
   if (language::instances -> contains (s)) return language (s);
   if (s == "american")   return make_text_language (s, "us");
   if (s == "british")    return make_text_language (s, "ukenglish");
   if (s == "bulgarian")  return make_ucs_text_language (s, "bulgarian");
-  if (s == "chinese")    return make_oriental_language (s);
+  if (s == "chinese")    return make_oriental_language(s);
   if (s == "croatian")   return make_text_language (s, "croatian");
   if (s == "czech")      return make_text_language (s, "czech");
   if (s == "danish")     return make_text_language (s, "danish");
@@ -503,7 +640,7 @@ text_language (string s) {
   if (s == "slovene")    return make_text_language (s, "slovene");
   if (s == "spanish")    return make_text_language (s, "spanish");
   if (s == "swedish")    return make_text_language (s, "swedish");
-  if (s == "chineset")  return make_oriental_language (s);
+  if (s == "chineset")  return make_chinese_language(s);
   if (s == "ukrainian")  return make_ucs_text_language (s, "ukrainian");
   if (s == "verbatim")   return tm_new<verb_language_rep> ("verbatim");
   failed_error << "The language was " << s << "\n";
