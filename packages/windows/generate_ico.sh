8446bd14884a312b494bad6ce2c1ed95d58f2fc9
Darcy Shen <sadhen@zoho.com>
2023-01-22 15:54:33 +0800

    Use new icon for Windows 15m (#507)
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

---
+++ packages/windows/generate_ico.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# convert TeXmacs/misc/images/xmacs-256.png -define icon:auto-resize=256,64,48,32,16 packages/windows/Xmacs.ico
+convert TeXmacs/misc/images/new-mogan-512.png -define icon:auto-resize=256,64,48,32,16 packages/windows/Xmacs.ico
+convert TeXmacs/misc/images/texmacs-512.png -define icon:auto-resize=256,64,48,32,16 packages/windows/TeXmacs.ico
