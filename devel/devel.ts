4ddd333a60040a0fd21a3bdba28210967919ffad
沈浪熊猫儿 <shenda@ustc.edu>
2023-05-02 17:33:07 +0800

    [1.2.0] Extract preamble to devel.ts

---
+++ devel/devel.ts
@@ -0,0 +1,26 @@
+<TeXmacs|2.1.2>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|devel|0.1>
+
+      <src-purpose|style for dev docs>
+    </src-title>
+  </active*>
+
+  <use-package|tmdoc|british>
+
+  <\active*>
+    <\src-comment>
+      Macro definitions.
+    </src-comment>
+  </active*>
+
+  <assign|dlink|<macro|name|<hlink|[<arg|name>]|<merge|<arg|name>|.tm>>>>
+</body>
+
+<initial|<\collection>
+</collection>>
