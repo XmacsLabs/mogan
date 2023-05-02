085dc3d729ddef5f7ffa22022461249a4d8b9b57
Darcy Shen <sadhen@zoho.com>
2023-05-02 23:32:43 +0800

    [4_2] UI: use Qt::MiddleButton
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

--- src/Plugins/Qt/QTMWidget.cpp
+++ src/Plugins/Qt/QTMWidget.cpp
@@ -465,11 +465,7 @@ mouse_state (QMouseEvent* event, bool flag) {
   Qt::KeyboardModifiers kstate= event->modifiers ();
   if (flag) bstate= bstate | tstate;
   if ((bstate & Qt::LeftButton     ) != 0) i += 1;
-#if QT_VERSION <  QT_VERSION_CHECK(6, 0, 0)
-  if ((bstate & Qt::MidButton      ) != 0) i += 2;
-#else
   if ((bstate & Qt::MiddleButton   ) != 0) i += 2;
-#endif
   if ((bstate & Qt::RightButton    ) != 0) i += 4;
   if ((bstate & Qt::XButton1       ) != 0) i += 8;
   if ((bstate & Qt::XButton2       ) != 0) i += 16;
@@ -633,11 +629,7 @@ tablet_state (QTabletEvent* event, bool flag) {
   Qt::MouseButton  tstate= event->button ();
   if (flag) bstate= bstate | tstate;
   if ((bstate & Qt::LeftButton     ) != 0) i += 1;
-#if QT_VERSION <  QT_VERSION_CHECK(6, 0, 0)
-  if ((bstate & Qt::MidButton      ) != 0) i += 2;
-#else
   if ((bstate & Qt::MiddleButton   ) != 0) i += 2;
-#endif
   if ((bstate & Qt::RightButton    ) != 0) i += 4;
   if ((bstate & Qt::XButton1       ) != 0) i += 8;
   if ((bstate & Qt::XButton2       ) != 0) i += 16;
@@ -921,11 +913,7 @@ wheel_state (QWheelEvent* event) {
   Qt::MouseButtons bstate= event->buttons ();
   Qt::KeyboardModifiers kstate= event->modifiers ();
   if ((bstate & Qt::LeftButton     ) != 0) i += 1;
-#if QT_VERSION <  QT_VERSION_CHECK(6, 0, 0)
-  if ((bstate & Qt::MidButton      ) != 0) i += 2;
-#else
   if ((bstate & Qt::MiddleButton      ) != 0) i += 2;
-#endif
   if ((bstate & Qt::RightButton    ) != 0) i += 4;
   if ((bstate & Qt::XButton1       ) != 0) i += 8;
   if ((bstate & Qt::XButton2       ) != 0) i += 16;
