522f412b8b3156befe8e8652e88a7041fd64f005
沈浪熊猫儿 <sadhen@zoho.com.cn>
2023-05-16 15:01:27 +0000

    [9_1] Fix corrupted metadata in PDF export

--- TeXmacs/tests/9_1.scm
+++ TeXmacs/tests/9_1.scm
@@ -10,10 +10,20 @@
   (regression-test-group
    "get-metadata" "result"
    get-metadata :none
-   (test "with title" "title" "<#6807><#9898>")))
+   (test "with title" "title" "标题")))
+
+(define (test-metadata)
+  (load-buffer "$TEXMACS_PATH/tests/9_1_with_metadata.tm")
+  (regression-test-group
+   "get-metadata" "result"
+   get-metadata :none
+   (test "get author" "author" "作者")
+   (test "get subject" "subject" "主题")
+   (test "get title" "title" "标题")))
 
 (tm-define (test_9_1)
   (let ((n (+ (test-no-title)
-              (test-title))))
+              (test-title)
+              (test-metadata))))
     (display* "Total: " (object->string n) " tests.\n")
     (display "Test suite of 9_1: ok\n")))
