a07c0442d8c8c1650c680b230e49031c258583af
Darcy Shen <darcy@noreply.codeberg.org>
2023-05-26 11:28:08 +0000

    [15_1] Add the license header of L1/L2 glue and object
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>
    Reviewed-on: https://codeberg.org/XmacsLabs/mogan/pulls/61

--- src/Scheme/L1/object_l1.hpp
+++ src/Scheme/L1/object_l1.hpp
@@ -1,3 +1,14 @@
+
+/******************************************************************************
+ * MODULE     : object_l1.hpp
+ * DESCRIPTION: Implementation of scheme objects (L1)
+ * COPYRIGHT  : (C) 1999-2011 Joris van der Hoeven and Massimiliano Gubinelli
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
 #ifndef OBJECT_L1_HPP
 #define OBJECT_L1_HPP
 
--- src/Scheme/L2/object_l2.hpp
+++ src/Scheme/L2/object_l2.hpp
@@ -1,3 +1,14 @@
+
+/******************************************************************************
+ * MODULE     : object_l2.hpp
+ * DESCRIPTION: Implementation of scheme objects (L2)
+ * COPYRIGHT  : (C) 1999-2011 Joris van der Hoeven and Massimiliano Gubinelli
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
 #ifndef OBJECT_L2_HPP
 #define OBJECT_L2_HPP
 
