507878a48685a0ba624b1e617ef45efe9612530a
Origin-yy <1657615110@qq.com>
2023-06-01 17:58:16 +0800

    [14_1]replenish tests for tree.cpp

--- tests/Kernel/Types/tree_test.cpp
+++ tests/Kernel/Types/tree_test.cpp
@@ -22,6 +22,12 @@ private slots:
   void test_is_compound ();
   void test_is_bool ();
   void test_is_int ();
+  void test_is_func ();
+  void test_is_double ();
+  void test_is_string ();
+  void test_is_generic ();
+  void test_get_label ();
+  void test_arity ();
 };
 
 void
@@ -69,6 +75,58 @@ TestTree::test_is_int () {
   QVERIFY (is_int (tree ("-12")));
   QVERIFY (!is_int (tree ("-+12")));
   QVERIFY (!is_int (tree ("one")));
+}
+
+void
+TestTree::test_is_func () {
+  QVERIFY (!is_func (tree (FRAC, 3),tree_label(DOCUMENT)));
+  QVERIFY (!is_func (tree (),tree_label()));
+  QVERIFY (is_func (tree (LSUP,4),tree_label(LSUP)));
+}
+
+void
+TestTree::test_is_double () {
+  QVERIFY (!is_double (tree ()));
+  QVERIFY (is_double (tree ("3.15")));
+  QVERIFY (!is_double (tree ("0")));
+  QVERIFY (is_double (tree ("0.0")));
+  QVERIFY (is_double (tree ("3.1415926")));
+  QVERIFY (is_double (tree ("-3.1415926")));
+  QVERIFY (!is_double (tree ("nothing")));
+}
+
+void
+TestTree::test_is_string () {
+  QVERIFY (is_string (tree ()));
+  QVERIFY (!is_string (tree ("0")));
+  QVERIFY (is_string (tree ("+121234")));
+  QVERIFY (is_string (tree ("seigj")));
+  QVERIFY (!is_string (tree ("!@#$$!")));
+  QVERIFY (!is_string (tree (FRAC, 3)));
+}
+
+void
+TestTree::test_is_generic () {
+  QVERIFY (!is_generic (tree (RIGID,0)));
+  QVERIFY (is_generic (tree (PARA,-1)));
+  QVERIFY (!is_generic (tree (RIGID,1234)));
+  QVERIFY (is_generic (tree (IF_PAGE_BREAK,-45)));
+  QVERIFY (!is_generic (tree (RIGID,2)));
+}
+
+void
+TestTree::test_get_label () {
+  QVERIFY (get_label (tree (RIGID,1)) == string("RIGID"));
+  QVERIFY (get_label (tree (ARC,0)) != string("RIGID"));
+  QVERIFY (get_label (tree (ARC,-1)) == string("ARC"));
+}
+
+void
+TestTree::test_arity () {
+  QVERIFY (!arity (tree (STRING, -1)));
+  QVERIFY (arity (tree (ARC, 0)));
+  QVERIFY (arity (tree (RIGID,3)));
+  QVERIFY (!arity (tree (STRING,5)));
 }
 
 QTEST_MAIN (TestTree)
