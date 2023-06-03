45d40b917b38e1c25a13e04bf6098215e12a7652
Darcy Shen <darcy@noreply.codeberg.org>
2023-06-03 13:39:49 +0000

    [14_6] bin/format on color_test
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>
    Reviewed-on: https://codeberg.org/XmacsLabs/mogan/pulls/74

--- tests/Graphics/Colors/color_test.cpp
+++ tests/Graphics/Colors/color_test.cpp
@@ -16,30 +16,30 @@ class TestColor : public QObject {
 
 private slots:
   void test_get_rgb_color ();
-  void test_rgb_color();
+  void test_rgb_color ();
   void test_cmyk_color ();
 };
 
-void 
-TestColor::test_rgb_color(){
-  array<int> myArray = {255,255,0};
-  color c = 0xFFFFFF00;
-  QVERIFY(rgb_color(255,255,0) == c);
+void
+TestColor::test_rgb_color () {
+  array<int> myArray= {255, 255, 0};
+  color      c      = 0xFFFFFF00;
+  QVERIFY (rgb_color (255, 255, 0) == c);
 }
 
-void 
-TestColor::test_get_rgb_color(){
-  array<int> myArray = {255,255,0,255};
-  color c = 0xFFFFFF00;
-  array<int> col_array(4);
-  get_rgb_color(c, col_array[0], col_array[1], col_array[2], col_array[3]);
-  QVERIFY(col_array == myArray);
+void
+TestColor::test_get_rgb_color () {
+  array<int> myArray= {255, 255, 0, 255};
+  color      c      = 0xFFFFFF00;
+  array<int> col_array (4);
+  get_rgb_color (c, col_array[0], col_array[1], col_array[2], col_array[3]);
+  QVERIFY (col_array == myArray);
 }
 
-void 
-TestColor::test_cmyk_color(){
-  color c = 0xFFFFFF00;
-  QVERIFY(cmyk_color(0,0,255,0) == c);
+void
+TestColor::test_cmyk_color () {
+  color c= 0xFFFFFF00;
+  QVERIFY (cmyk_color (0, 0, 255, 0) == c);
 }
 
 QTEST_MAIN (TestColor)
