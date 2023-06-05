68b88f4974f3f3a8b97da826d5b20bb997beb088
Darcy Shen <darcy@noreply.codeberg.org>
2023-06-05 13:47:12 +0000

    bump to 1.1.3
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>
    Reviewed-on: https://codeberg.org/XmacsLabs/mogan/pulls/86

--- xmake.lua
+++ xmake.lua
@@ -70,7 +70,7 @@ if not is_plat("wasm") then
     add_requires("nowide_standalone 11.2.0", {system=false})
 end
 
-local XMACS_VERSION="1.1.3-rc1"
+local XMACS_VERSION="1.1.3"
 local INSTALL_DIR="build/package"
 
 local TEXMACS_VERSION = "2.1.2"
