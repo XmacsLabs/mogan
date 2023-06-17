52dff3168e3205d734acb0eafd4fb4726a42770f
Darcy Shen <sadhen@zoho.com>
2023-06-17 13:35:19 +0800

    [15_3_11] glue_bibtex
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

--- src/Scheme/Glue/glue_basic.lua
+++ src/Scheme/Glue/glue_basic.lua
@@ -281,14 +281,6 @@ function main()
                 }
             },
             {
-                scm_name = "set-bibtex-command",
-                cpp_name = "set_bibtex_command",
-                ret_type = "void",
-                arg_list = {
-                    "string"
-                }
-            },
-            {
                 scm_name = "number-latex-errors",
                 cpp_name = "number_latex_errors",
                 ret_type = "int",
@@ -2176,34 +2168,6 @@ function main()
                 }
             },
             {
-                scm_name = "parse-bib",
-                cpp_name = "parse_bib",
-                ret_type = "tree",
-                arg_list = {
-                    "string"
-                }
-            },
-            {
-                scm_name = "conservative-bib-import",
-                cpp_name = "conservative_bib_import",
-                ret_type = "tree",
-                arg_list = {
-                    "string",
-                    "content",
-                    "string"
-                }
-            },
-            {
-                scm_name = "conservative-bib-export",
-                cpp_name = "conservative_bib_export",
-                ret_type = "string",
-                arg_list = {
-                    "content",
-                    "string",
-                    "content"
-                }
-            },
-            {
                 scm_name = "clean-html",
                 cpp_name = "clean_html",
                 ret_type = "tree",
@@ -3699,133 +3663,6 @@ function main()
                 ret_type = "path",
                 arg_list = {
                     "url"
-                }
-            },
-            
-              -- routines for BibTeX
-            {
-                scm_name = "supports-bibtex?",
-                cpp_name = "bibtex_present",
-                ret_type = "bool"
-            },
-            {
-                scm_name = "bibtex-run",
-                cpp_name = "bibtex_run",
-                ret_type = "tree",
-                arg_list = {
-                    "string",
-                    "string",
-                    "url",
-                    "array_string"
-                }
-            },
-            {
-                scm_name = "bib-add-period",
-                cpp_name = "bib_add_period",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-locase-first",
-                cpp_name = "bib_locase_first",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-upcase-first",
-                cpp_name = "bib_upcase_first",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-locase",
-                cpp_name = "bib_locase",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-upcase",
-                cpp_name = "bib_upcase",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-default-preserve-case",
-                cpp_name = "bib_default_preserve_case",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-default-upcase-first",
-                cpp_name = "bib_default_upcase_first",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-purify",
-                cpp_name = "bib_purify",
-                ret_type = "string",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-text-length",
-                cpp_name = "bib_text_length",
-                ret_type = "int",
-                arg_list = {
-                    "scheme_tree"
-                }
-            },
-            {
-                scm_name = "bib-prefix",
-                cpp_name = "bib_prefix",
-                ret_type = "string",
-                arg_list = {
-                    "scheme_tree",
-                    "int"
-                }
-            },
-            {
-                scm_name = "bib-empty?",
-                cpp_name = "bib_empty",
-                ret_type = "bool",
-                arg_list = {
-                    "scheme_tree",
-                    "string"
-                }
-            },
-            {
-                scm_name = "bib-field",
-                cpp_name = "bib_field",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree",
-                    "string"
-                }
-            },
-            {
-                scm_name = "bib-abbreviate",
-                cpp_name = "bib_abbreviate",
-                ret_type = "scheme_tree",
-                arg_list = {
-                    "scheme_tree",
-                    "scheme_tree",
-                    "scheme_tree"
                 }
             },
         }
