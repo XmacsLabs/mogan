31c682375abceb2d780fb9df933a737a74dbfd35
Darcy Shen <sadhen@zoho.com>
2023-06-17 22:14:01 +0800

    [15_3_15] Move glues to src/Scheme/Plugins
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

---
+++ src/Scheme/Plugins/init_glue_plugins.hpp
@@ -0,0 +1,19 @@
+
+/******************************************************************************
+* MODULE     : glue_l3.hpp
+* DESCRIPTION: L3 Glue for linking TeXmacs commands to scheme
+* COPYRIGHT  : (C) 1999-2011  Joris van der Hoeven and Massimiliano Gubinelli
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
+#ifndef INIT_GLUE_PLUGINS_HPP
+#define INIT_GLUE_PLUGINS_HPP
+
+void initialize_glue_plugins ();
+
+#endif
+
+
