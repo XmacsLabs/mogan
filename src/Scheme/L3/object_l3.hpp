d0f4c9d6dbe0bc77da143ba923bbe4dbb617a7c1
Darcy Shen <sadhen@zoho.com>
2023-06-19 23:23:54 +0800

    [15_5] Re-org on L1/L2/L3 Kernel part4
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

--- src/Scheme/L3/object_l3.hpp
+++ src/Scheme/L3/object_l3.hpp
@@ -25,6 +25,12 @@ bool         tmscm_is_array_patch (tmscm p);
 array<patch> tmscm_to_array_patch (tmscm p);
 tmscm        array_patch_to_tmscm (array<patch> a);
 
+tmscm    observer_to_tmscm (observer o);
+observer tmscm_to_observer (tmscm obj);
+bool     tmscm_is_observer (tmscm o);
+
+#define TMSCM_ASSERT_OBSERVER(o, arg, rout)                                    \
+  TMSCM_ASSERT (tmscm_is_observer (o), o, arg, rout)
 #define TMSCM_ASSERT_ARRAY_PATCH(p, arg, rout)                                 \
   TMSCM_ASSERT (tmscm_is_array_patch (p), p, arg, rout)
 
