4b7be29cf819eafec8e2cde8cfd64b2adf144056
Darcy Shen <sadhen@zoho.com>
2023-06-19 23:59:05 +0800

    [15_5] Re-org on L1/L2/L3 Kernel part5
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

--- src/Scheme/L3/init_glue_l3.cpp
+++ src/Scheme/L3/init_glue_l3.cpp
@@ -56,6 +56,7 @@ patchP (tmscm t) {
 
 #include "glue_modification.cpp"
 #include "glue_patch.cpp"
+#include "glue_path.cpp"
 #include "glue_string.cpp"
 
 void
@@ -63,6 +64,7 @@ initialize_glue_l3 () {
   tmscm_install_procedure ("patch?", patchP, 1, 0, 0);
 
   initialize_glue_string ();
+  initialize_glue_path ();
   initialize_glue_modification ();
   initialize_glue_patch ();
 }
