cba2b01fc4f4ff2e9d4ca0fcb1555d1d5135c14a
Yiheng Wu <36156959+jingkaimori@users.noreply.github.com>
2023-07-25 22:45:22 +0800

    [3_29] separate xrepo package from xmake build cache

---
+++ misc/xmake/packages.lua
@@ -0,0 +1,62 @@
+-------------------------------------------------------------------------------
+--
+-- MODULE      : packages.lua
+-- DESCRIPTION : Xmake package config file for TeXmacs
+-- COPYRIGHT   : (C) 2023       jingkaimori
+--                   2022-2023  Darcy Shen
+--
+-- This software falls under the GNU general public license version 3 or later.
+-- It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+-- in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+
+--
+-- Dependencies: Platform|Package Manager
+--
+
+-- GNU/Linux variants
+-- [x] APT powered
+-- [ ] pacman powered
+-- [ ] portage powered
+-- ...
+function add_requires_of_mogan()
+    if is_plat("linux") and (linuxos.name() == "ubuntu" or linuxos.name() == "uos") then
+        add_requires("apt::libcurl4-openssl-dev", {alias="libcurl"})
+        add_requires("apt::libpng-dev", {alias="libpng"})
+        add_requires("apt::zlib1g-dev", {alias="zlib"})
+        -- config package name for libjpeg on Ubuntu
+        if linuxos.name() == "ubuntu" then
+            add_requires("apt::libjpeg-turbo8-dev", {alias="libjpeg"})
+        else
+            add_requires("apt::libjpeg62-turbo-dev", {alias="libjpeg"})
+        end
+        -- config package name for freetype on UOS
+        if linuxos.name() == "uos" then
+            add_requires("apt::libfreetype6-dev", {alias="freetype"})
+        else
+            add_requires("apt::libfreetype-dev", {alias="freetype"})
+        end
+    else
+    -- Let xrepo manage the dependencies for macOS and other GNU/Linux distros
+        add_requires("libpng 1.6.37", {system=false})
+        add_requires("libiconv 1.17", {system=false})
+        add_requires("zlib 1.2.12", {system=false})
+        add_requires("libjpeg v9e", {system=false})
+        add_requires("libcurl 7.84.0", {system=false})
+        add_requires("freetype 2.12.1", {system=false})
+    end
+
+    if is_plat("mingw") then
+        add_requires("nowide_standalone 11.2.0", {system=false})
+        add_requires("qt5widgets 5.15.2")
+        add_requires("qtifw 4.6.0")
+    end
+
+    if is_plat("linux") then
+        add_requires("fontconfig", {system = true})
+    end
+
+    local PDFHUMMUS_VERSION = "4.5.6"
+    set_configvar("PDFHUMMUS_VERSION", PDFHUMMUS_VERSION)
+    add_requires("pdfhummus "..PDFHUMMUS_VERSION, {system=false,configs={libpng=true,libjpeg=true}})
+    add_requires("s7 2023.04.13", {system=false})
+end
