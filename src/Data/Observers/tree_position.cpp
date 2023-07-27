db76bee8244837d8b777adf861bbb50c5687d563
沈浪熊猫儿 <shenda@ustc.edu>
2023-07-28 00:08:50 +0800

    fix

--- src/Data/Observers/tree_position.cpp
+++ src/Data/Observers/tree_position.cpp
@@ -12,7 +12,6 @@
 * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 ******************************************************************************/
 
-#include "iterator.hpp"
 #include "tree.hpp"
 #include "path.hpp"
 
