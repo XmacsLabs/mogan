2afaa1cefabae095ded265db8ebef562f795916f
沈浪熊猫儿 <shenda@ustc.edu>
2023-07-29 15:52:12 +0800

    [28_8] Use lolly 0.99.5

---
+++ src/Graphics/Gui/gui.cpp
@@ -0,0 +1,30 @@
+
+/******************************************************************************
+* MODULE     : gui.cpp
+* DESCRIPTION: several gui related routines
+* COPYRIGHT  : (C) 1999  Joris van der Hoeven
+*******************************************************************************
+* This software falls under the GNU general public license version 3 or later.
+* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+******************************************************************************/
+
+#include "gui.hpp"
+
+bool
+gui_is_x () {
+#ifdef QTTEXMACS
+  return false;
+#else
+  return true;
+#endif
+}
+
+bool
+gui_is_qt () {
+#ifdef QTTEXMACS
+  return true;
+#else
+  return false;
+#endif
+}
