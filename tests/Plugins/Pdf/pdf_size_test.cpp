fe618fd3f6aecf621739112ddfe54126b8ae70bf
tangdouer1005 <884746853@qq.com>
2023-07-29 11:26:22 +0800

    [26_2] add test on hummus_pdf_image_size

--- tests/Plugins/Pdf/pdf_size_test.cpp
+++ tests/Plugins/Pdf/pdf_size_test.cpp
@@ -24,6 +24,23 @@ TestHummusPdfImageSize::test_hummus_pdf_image_size () {
   hummus_pdf_image_size (url ("$TEXMACS_PATH/tests/images/26_2_1.pdf"), w, h);
   QCOMPARE (w, 595);
   QCOMPARE (h, 595);
+
+  w= h= -1;
+  hummus_pdf_image_size (url ("$TEXMACS_PATH/tests/images/26_2_2pages.pdf"), w,
+                         h);
+  QCOMPARE (w, 595);
+  QCOMPARE (h, 841);
+
+  w= h= -1;
+  hummus_pdf_image_size (url ("$TEXMACS_PATH/tests/images/26_2_3wrong.txt"), w,
+                         h);
+  QCOMPARE (w, 0);
+  QCOMPARE (h, 0);
+
+  w= h= -1;
+  hummus_pdf_image_size (url ("$TEXMACS_PATH/tests/images/null.pdf"), w, h);
+  QCOMPARE (w, 0);
+  QCOMPARE (h, 0);
 }
 
 QTEST_MAIN (TestHummusPdfImageSize)
