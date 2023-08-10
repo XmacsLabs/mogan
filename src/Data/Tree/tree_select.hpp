94638e3a56c04623c529215ab1059e26a11af743
Ye Tianxin <1656746727@qq.com>
2023-08-10 22:18:25 +0800

    [16_4] add linter github action

--- src/Data/Tree/tree_select.hpp
+++ src/Data/Tree/tree_select.hpp
@@ -1,13 +1,13 @@
 
 /******************************************************************************
-* MODULE     : tree_select.hpp
-* DESCRIPTION: abstract cursor handling
-* COPYRIGHT  : (C) 1999  Joris van der Hoeven
-*******************************************************************************
-* This software falls under the GNU general public license version 3 or later.
-* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
-* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
-******************************************************************************/
+ * MODULE     : tree_select.hpp
+ * DESCRIPTION: abstract cursor handling
+ * COPYRIGHT  : (C) 1999  Joris van der Hoeven
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
 
 #ifndef TREE_SELECT_H
 #define TREE_SELECT_H
@@ -24,15 +24,15 @@ path table_search_format (tree t, path p);
 void table_search_coordinates (tree t, path p, int& row, int& col);
 path table_search_cell (tree t, int row, int col);
 bool is_table_selection (tree et, path p1, path p2, bool strict);
-path find_subtable_selection (tree et, path p1, path p2,
-                              int& row1, int& col1, int& row2, int& col2);
+path find_subtable_selection (tree et, path p1, path p2, int& row1, int& col1,
+                              int& row2, int& col2);
 
 typedef array<path> range_set;
-range_set no_ranges ();
-range_set simple_range (path start, path end);
-bool is_empty (range_set sel);
-path start (range_set sel);
-path end (range_set sel);
-path common (range_set sel);
+range_set           no_ranges ();
+range_set           simple_range (path start, path end);
+bool                is_empty (range_set sel);
+path                start (range_set sel);
+path                end (range_set sel);
+path                common (range_set sel);
 
 #endif // defined TREE_SELECT_H
--- src/Graphics/Types/point.hpp
+++ src/Graphics/Types/point.hpp
@@ -1,13 +1,13 @@
 
 /******************************************************************************
-* MODULE     : point.hpp
-* DESCRIPTION: points
-* COPYRIGHT  : (C) 2003  Joris van der Hoeven
-*******************************************************************************
-* This software falls under the GNU general public license version 3 or later.
-* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
-* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
-******************************************************************************/
+ * MODULE     : point.hpp
+ * DESCRIPTION: points
+ * COPYRIGHT  : (C) 2003  Joris van der Hoeven
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
 
 #ifndef POINT_H
 #define POINT_H
@@ -15,22 +15,26 @@
 
 typedef array<double> point;
 
-point operator -  (point p);
-point operator +  (point p1, point p2);
-point operator -  (point p1, point p2);
-point operator *  (double x, point p );
-point operator *  (point p1, point p2);
-point operator /  (point  p, double x);
-point operator /  (point p1, point p2);
-bool  operator == (point p1, point p2);
+point operator- (point p);
+point operator+ (point p1, point p2);
+point operator- (point p1, point p2);
+point operator* (double x, point p);
+point operator* (point p1, point p2);
+point operator/ (point p, double x);
+point operator/ (point p1, point p2);
+bool  operator== (point p1, point p2);
 
-point abs (point p);
+point  abs (point p);
 double min (point p);
 double max (point p);
 
 bool is_point (tree t);
-inline point as_point(double x) {
-  point p(1); p[0]=x; return p; }
+inline point
+as_point (double x) {
+  point p (1);
+  p[0]= x;
+  return p;
+}
 point as_point (tree t);
 tree  as_tree (point p);
 
@@ -42,15 +46,17 @@ double norm (point p);
 double arg (point p);
 bool   collinear (point p1, point p2);
 bool   linearly_dependent (point p1, point p2, point p3);
-bool   orthogonalize (point &i, point &j, point p1, point p2, point p3);
+bool   orthogonalize (point& i, point& j, point p1, point p2, point p3);
 
-typedef struct { point p0, p1; } axis;
+typedef struct {
+  point p0, p1;
+} axis;
 
-point  proj         (axis a, point p);
-double dist         (axis a, point p);
-double seg_dist     (axis a, point p);
-double seg_dist     (point p1, point p2, point p);
-axis   midperp      (point p1, point p2, point p3);
+point  proj (axis a, point p);
+double dist (axis a, point p);
+double seg_dist (axis a, point p);
+double seg_dist (point p1, point p2, point p);
+axis   midperp (point p1, point p2, point p3);
 point  intersection (axis A, axis B);
 
 bool inside_rectangle (point p, point p1, point p2);
