55a047ca0a83c925083abf183006db82d8893311
charonxin <1656746727@qq.com>
2023-08-10 15:33:31 +0800

    fix test/**/*.hpp

--- tests/Base/base.hpp
+++ tests/Base/base.hpp
@@ -1,14 +1,13 @@
 
 /******************************************************************************
-* MODULE     : base.hpp
-* DESCRIPTION: header file for test purpose
-* COPYRIGHT  : (C) 2022  Darcy Shen
-*******************************************************************************
-* This software falls under the GNU general public license version 3 or later.
-* It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
-* in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
-******************************************************************************/
-
+ * MODULE     : base.hpp
+ * DESCRIPTION: header file for test purpose
+ * COPYRIGHT  : (C) 2022  Darcy Shen
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
 
 #ifndef TM_TEST_BASE_HPP
 #define TM_TEST_BASE_HPP
