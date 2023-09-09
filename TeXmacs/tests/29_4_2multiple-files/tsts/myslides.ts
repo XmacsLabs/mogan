5201da7cd0d0b8de9fc46904d7b5d37911fd486c
Tangdouer <884746853@qq.com>
2023-09-09 17:53:49 +0800

    [29_4] add test for making sigle and multiple attachments

---
+++ TeXmacs/tests/29_4_2multiple-files/tsts/myslides.ts
@@ -0,0 +1,37 @@
+<TeXmacs|2.1.2>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|my-slides-macros|1.0>
+
+      <src-purpose|Automatically generated style file.>
+    </src-title>
+  </active*>
+
+  <use-package|seminar>
+
+  <\active*>
+    <\src-comment>
+      Style parameters.
+    </src-comment>
+  </active*>
+
+  <assign|font|stix>
+
+  <assign|font-base-size|9>
+
+  <assign|font-family|rm>
+
+  <assign|full-screen-mode|false>
+
+  <assign|info-flag|none>
+
+  <assign|magnification|2>
+
+</body>
+
+<initial|<\collection>
+</collection>>
--- tests/Plugins/Pdf/pdf_make_attachment_test.cpp
+++ tests/Plugins/Pdf/pdf_make_attachment_test.cpp
@@ -9,6 +9,7 @@
  ******************************************************************************/
 
 #include "file.hpp"
+#include "pdf_hummus_extract_attachment.hpp"
 #include "pdf_hummus_make_attachment.hpp"
 #include "sys_utils.hpp"
 #include <QtTest/QtTest>
@@ -18,21 +19,74 @@ class TestHummusPdfMakeAttachment : public QObject {
 
 private slots:
   void init () { lolly::init_tbox (); }
-  void test_pdf_hummus_make_attachment ();
+  void test_pdf_hummus_make_single_attachment ();
+  void test_pdf_hummus_make_multiple_attachments ();
 };
-
 void
-TestHummusPdfMakeAttachment::test_pdf_hummus_make_attachment () {
-
+TestHummusPdfMakeAttachment::test_pdf_hummus_make_single_attachment () {
   bool attach_judge= pdf_hummus_make_attachments (
       url ("$TEXMACS_PATH/tests/images/29_1_1.pdf"),
       list<url> (url ("$TEXMACS_PATH/tests/29_1_1.tm")),
       url ("$TEXMACS_PATH/tests/images/29_1_1_attach.pdf"));
   QVERIFY (attach_judge);
-
   bool out_pdf_judge=
       is_regular (url ("$TEXMACS_PATH/tests/images/29_1_1_attach.pdf"));
   QVERIFY (out_pdf_judge);
+
+  list<url> attachment;
+  bool      separate_tm_judge= extract_attachments_from_pdf (
+      url ("$TEXMACS_PATH/tests/images/29_1_1_attach.pdf"), attachment);
+  QVERIFY (separate_tm_judge);
+
+  QVERIFY (N (attachment) == 1);
+
+  bool tm_exist_judge= is_regular (attachment[0]);
+  QVERIFY (tm_exist_judge);
+
+  QVERIFY (attachment[0] == url ("$TEXMACS_PATH/tests/images/29_1_1.tm"));
+
+  remove (url ("$TEXMACS_PATH/tests/images/29_1_1_attach.pdf"));
+  remove (attachment[0]);
+}
+
+void
+TestHummusPdfMakeAttachment::test_pdf_hummus_make_multiple_attachments () {
+
+  auto multiple_tm= list<url> (
+      url ("$TEXMACS_PATH/tests/29_4_2multiple-files/main.tm"),
+      url ("$TEXMACS_PATH/tests/29_4_2multiple-files/tsts/myslides.ts"),
+      url ("$TEXMACS_PATH/tests/29_4_2multiple-files/p/logo.pdf"));
+
+  bool attach_judge= pdf_hummus_make_attachments (
+      url ("$TEXMACS_PATH/tests/images/29_4_2.pdf"), multiple_tm,
+      url ("$TEXMACS_PATH/tests/images/29_4_2_attach.pdf"));
+  QVERIFY (attach_judge);
+
+  bool out_pdf_judge=
+      is_regular (url ("$TEXMACS_PATH/tests/images/29_4_2_attach.pdf"));
+  QVERIFY (out_pdf_judge);
+
+  list<url> attachments;
+  bool      separate_tm_judge= extract_attachments_from_pdf (
+      url ("$TEXMACS_PATH/tests/images/29_4_2_attach.pdf"), attachments);
+  QVERIFY (separate_tm_judge);
+
+  QVERIFY (N (attachments) == 3);
+  for (int i= 0; i < N (attachments); i++) {
+    bool tm_exist_judge= is_regular (attachments[i]);
+    QVERIFY (tm_exist_judge);
+  }
+
+  QVERIFY (contains (attachments, url ("$TEXMACS_PATH/tests/images/main.tm")));
+  QVERIFY (
+      contains (attachments, url ("$TEXMACS_PATH/tests/images/myslides.ts")));
+  QVERIFY (contains (attachments, url ("$TEXMACS_PATH/tests/images/logo.pdf")));
+
+  remove (url ("$TEXMACS_PATH/tests/images/29_4_2_attach.pdf"));
+
+  for (int i= 0; i < N (attachments); i++) {
+    remove (attachments[i]);
+  }
 }
 
 QTEST_MAIN (TestHummusPdfMakeAttachment)
