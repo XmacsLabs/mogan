5465d602daecacd5d9175f4d7be268a7ce5f6d09
Paradisum <2513209473@qq.com>
2023-09-19 13:03:41 +0800

    fix

--- tests/Plugins/Database/database_basic_function_test.cpp
+++ tests/Plugins/Database/database_basic_function_test.cpp
@@ -8,6 +8,8 @@
  ******************************************************************************/
 
 #include "Database/database.hpp"
+#include "analyze.hpp"
+#include "file.hpp"
 #include "sys_utils.hpp"
 #include "tm_timer.hpp"
 #include "tree_helper.hpp"
@@ -30,67 +32,71 @@ private slots:
 
 void
 TestDatabaseBasicFunciton::test_set_field_and_get_field () {
-  url test_db= url_system ("db1");
+  url test_db= url_temp ("db1");
   // test single string
-  set_field (test_db, "single", "", array<string> ("", 1),
+  string single_val1[1]= {"defg"};
+  string single_val2[1]= {"5678"};
+  string single_val3[1]= {"defg5678"};
+  string single_val4[1]= {""};
+
+  set_field (test_db, "single", "abc", array<string> (single_val1, 1),
              (double) get_sec_time ());
-  set_field (test_db, "single", "a", array<string> ("b", 1),
+  set_field (test_db, "single", "1234", array<string> (single_val2, 1),
              (double) get_sec_time ());
-  set_field (test_db, "single", "1", array<string> ("2b", 1),
+  set_field (test_db, "single", "abc1234", array<string> (single_val3, 1),
              (double) get_sec_time ());
-  set_field (test_db, "single", "cd", array<string> ("3a", 1),
+  set_field (test_db, "single", "", array<string> (single_val4, 1),
              (double) get_sec_time ());
 
   array<string> s1=
-      get_field (test_db, "single", "a", (double) get_sec_time ());
+      get_field (test_db, "single", "abc", (double) get_sec_time ());
   array<string> s2=
-      get_field (test_db, "single", "1", (double) get_sec_time ());
+      get_field (test_db, "single", "1234", (double) get_sec_time ());
   array<string> s3=
-      get_field (test_db, "single", "cd", (double) get_sec_time ());
+      get_field (test_db, "single", "abc1234", (double) get_sec_time ());
   array<string> s4= get_field (test_db, "single", "", (double) get_sec_time ());
 
-  QVERIFY (s1[0] == "b");
-  QVERIFY (s2[0] == "2b");
-  QVERIFY (s3[0] == "3a");
+  QVERIFY (s1[0] == "defg");
+  QVERIFY (s2[0] == "5678");
+  QVERIFY (s3[0] == "defg5678");
   QVERIFY (s4[0] == "");
 
   // test multiple string
-  set_field (test_db, "multiple", "a1", array<string> ("b", "c"),
+  set_field (test_db, "multiple", "teacher", array<string> ("Bob", "Alice"),
              (double) get_sec_time ());
-  set_field (test_db, "multiple", "1b", array<string> ("2b", "23bb"),
+  set_field (test_db, "multiple", "number", array<string> ("123", "456"),
              (double) get_sec_time ());
-  set_field (test_db, "multiple", "345asd",
-             array<string> ("3a", "213fd", "wffwafw"),
+  set_field (test_db, "multiple", "room", array<string> ("A1", "B2", "C3"),
              (double) get_sec_time ());
 
   array<string> s5=
-      get_field (test_db, "multiple", "a1", (double) get_sec_time ());
+      get_field (test_db, "multiple", "teacher", (double) get_sec_time ());
   array<string> s6=
-      get_field (test_db, "multiple", "1b", (double) get_sec_time ());
+      get_field (test_db, "multiple", "number", (double) get_sec_time ());
   array<string> s7=
-      get_field (test_db, "multiple", "345asd", (double) get_sec_time ());
+      get_field (test_db, "multiple", "room", (double) get_sec_time ());
 
-  QVERIFY (s5 == array<string> ("b", "c"));
-  QVERIFY (s6 == array<string> ("2b", "23bb"));
-  QVERIFY (s7 == array<string> ("3a", "213fd", "wffwafw"));
+  QVERIFY (s5 == array<string> ("Bob", "Alice"));
+  QVERIFY (s6 == array<string> ("123", "456"));
+  QVERIFY (s7 == array<string> ("A1", "B2", "C3"));
 }
 
 void
 TestDatabaseBasicFunciton::test_remove_field () {
-  url test_db= url_system ("db2");
+  url test_db= url_temp ("db2");
 
-  set_field (test_db, "remove", "a", array<string> ("b", 1),
+  set_field (test_db, "remove", "no1", array<string> ("no1_name", "1"),
              (double) get_sec_time ());
-  set_field (test_db, "remove", "1", array<string> ("2b", 1),
+  set_field (test_db, "remove", "no2", array<string> ("no2_name", "2"),
              (double) get_sec_time ());
-  set_field (test_db, "remove", "cd", array<string> ("3a", 1),
+  set_field (test_db, "remove", "no3", array<string> ("no3_name", "3"),
              (double) get_sec_time ());
-  set_field (test_db, "remove", "a1", array<string> ("b", "c"),
+  set_field (test_db, "remove", "no4", array<string> ("no4_name", "4"),
              (double) get_sec_time ());
 
-  remove_field (test_db, "remove", "a", (double) get_sec_time ());
-  remove_field (test_db, "remove", "cd", (double) get_sec_time ());
-  remove_field (test_db, "remove", "1", (double) get_sec_time ());
+  remove_field (test_db, "remove", "no1", (double) get_sec_time ());
+  remove_field (test_db, "remove", "no3", (double) get_sec_time ());
+  remove_field (test_db, "remove", "no4", (double) get_sec_time ());
 
   // clean and write to disk
   keep_history (test_db, true);
@@ -98,74 +104,72 @@ TestDatabaseBasicFunciton::test_remove_field () {
   db_index->reset (url (test_db)->t);
 
   array<string> s1=
-      get_field (test_db, "remove", "a", (double) get_sec_time ());
+      get_field (test_db, "remove", "no1", (double) get_sec_time ());
   array<string> s2=
-      get_field (test_db, "remove", "1", (double) get_sec_time ());
+      get_field (test_db, "remove", "no2", (double) get_sec_time ());
   array<string> s3=
-      get_field (test_db, "remove", "cd", (double) get_sec_time ());
+      get_field (test_db, "remove", "no3", (double) get_sec_time ());
   array<string> s4=
-      get_field (test_db, "remove", "a1", (double) get_sec_time ());
+      get_field (test_db, "remove", "no4", (double) get_sec_time ());
 
   QVERIFY (N (s1) == 0);
-  QVERIFY (N (s2) == 0);
   QVERIFY (N (s3) == 0);
-  QVERIFY (s4 == array<string> ("b", "c"));
+  QVERIFY (N (s4) == 0);
+  QVERIFY (s2 == array<string> ("no2_name", "2"));
 }
 
 void
 TestDatabaseBasicFunciton::test_set_entry_and_get_entry () {
-  url test_db= url_system ("db3");
+  url test_db= url_temp ("db3");
 
-  tree t1 (TUPLE, tree (TUPLE, "a", "a1"));
-  tree t2 (TUPLE, tree (TUPLE, "b1", "bb11"), tree (TUPLE, "b2", "bb22"));
-  tree t3 (TUPLE, tree (TUPLE, "c1", "cc11"), tree (TUPLE, "c2", "cc22"),
-           tree (TUPLE, "c3", "cc33"));
+  tree t1= tuple (tuple ("camera", "Sony"));
+  tree t2= tuple (tuple ("phone", "huawei"), tuple ("car", "BYD"));
+  tree t3= tuple (tuple ("phone", "apple"), tuple ("mall", "Sam"));
 
-  set_entry (test_db, "entry1", t1, (double) get_sec_time ());
-  set_entry (test_db, "entry2", t2, (double) get_sec_time ());
-  set_entry (test_db, "entry3", t3, (double) get_sec_time ());
+  set_entry (test_db, "Japan", t1, (double) get_sec_time ());
+  set_entry (test_db, "China", t2, (double) get_sec_time ());
+  set_entry (test_db, "USA", t3, (double) get_sec_time ());
 
   // from 245 (245 (a, a1)) to 245 (245 ("a", "a1"))
-  tree t4= get_entry (test_db, "entry1", (double) get_sec_time ());
-  tree t5= get_entry (test_db, "entry2", (double) get_sec_time ());
-  tree t6= get_entry (test_db, "entry3", (double) get_sec_time ());
+  tree t4= get_entry (test_db, "Japan", (double) get_sec_time ());
+  tree t5= get_entry (test_db, "China", (double) get_sec_time ());
+  tree t6= get_entry (test_db, "USA", (double) get_sec_time ());
 
-  QVERIFY (t4 == tree (TUPLE, tree (TUPLE, "\"a\"", "\"a1\"")));
-  QVERIFY (t5 == tree (TUPLE, tree (TUPLE, "\"b1\"", "\"bb11\""),
-                       tree (TUPLE, "\"b2\"", "\"bb22\"")));
-  QVERIFY (t6 == tree (TUPLE, tree (TUPLE, "\"c1\"", "\"cc11\""),
-                       tree (TUPLE, "\"c2\"", "\"cc22\""),
-                       tree (TUPLE, "\"c3\"", "\"cc33\"")));
+  QVERIFY (t4 == tuple (tuple ("\"camera\"", "\"Sony\"")));
+  QVERIFY (t5 == tuple (tuple ("\"phone\"", "\"huawei\""),
+                        tuple ("\"car\"", "\"BYD\"")));
+  QVERIFY (t6 == tuple (tuple ("\"phone\"", "\"apple\""),
+                        tuple ("\"mall\"", "\"Sam\"")));
 }
 
 void
 TestDatabaseBasicFunciton::test_remove_entry () {
-  url test_db= url_system ("db4");
+  url test_db= url_temp ("db4");
 
-  tree t1 (TUPLE, tree (TUPLE, "a", "a1"));
-  tree t2 (TUPLE, tree (TUPLE, "b1", "bb11"), tree (TUPLE, "b2", "bb22"));
-  tree t3 (TUPLE, tree (TUPLE, "c1", "cc11"), tree (TUPLE, "c2", "cc22"),
-           tree (TUPLE, "c3", "cc33"));
+  tree t1= tuple (tuple ("camera", "Sony"));
+  tree t2= tuple (tuple ("phone", "huawei"), tuple ("car", "BYD"));
+  tree t3= tuple (tuple ("phone", "apple"), tuple ("mall", "Sam"));
 
-  set_entry (test_db, "entryre1", t1, (double) get_sec_time ());
-  set_entry (test_db, "entryre2", t2, (double) get_sec_time ());
-  set_entry (test_db, "entryre3", t3, (double) get_sec_time ());
+  set_entry (test_db, "Japan", t1, (double) get_sec_time ());
+  set_entry (test_db, "China", t2, (double) get_sec_time ());
+  set_entry (test_db, "USA", t3, (double) get_sec_time ());
 
   // clean and write to disk
   keep_history (test_db, true);
   keep_history (test_db, false);
   db_index->reset (url (test_db)->t);
 
-  remove_entry (test_db, "entryre1", (double) get_sec_time ());
-  remove_entry (test_db, "entryre3", (double) get_sec_time ());
+  remove_entry (test_db, "Japan", (double) get_sec_time ());
+  remove_entry (test_db, "USA", (double) get_sec_time ());
 
-  tree t4= get_entry (test_db, "entryre1", (double) get_sec_time ());
-  tree t5= get_entry (test_db, "entryre2", (double) get_sec_time ());
-  tree t6= get_entry (test_db, "entryre3", (double) get_sec_time ());
+  // from 245 (245 (a, a1)) to 245 (245 ("a", "a1"))
+  tree t4= get_entry (test_db, "Japan", (double) get_sec_time ());
+  tree t5= get_entry (test_db, "China", (double) get_sec_time ());
+  tree t6= get_entry (test_db, "USA", (double) get_sec_time ());
 
   QVERIFY (N (t4) == 0);
-  QVERIFY (t5 == tree (TUPLE, tree (TUPLE, "\"b1\"", "\"bb11\""),
-                       tree (TUPLE, "\"b2\"", "\"bb22\"")));
+  QVERIFY (t5 == tuple (tuple ("\"phone\"", "\"huawei\""),
+                        tuple ("\"car\"", "\"BYD\"")));
   QVERIFY (N (t6) == 0);
 }
 
@@ -173,43 +177,73 @@ void
 TestDatabaseBasicFunciton::test_get_attributes () {
   url test_db= url_system ("db5");
 
-  set_field (test_db, "attributes", "a1", array<string> ("b", "c"),
+  set_field (test_db, "attributes", "age", array<string> ("1", "18"),
              (double) get_sec_time ());
-  set_field (test_db, "attributes", "1b", array<string> ("2b", "23bb"),
+  set_field (test_db, "attributes", "name", array<string> ("bob", "black"),
              (double) get_sec_time ());
-  set_field (test_db, "attributes", "345asd",
-             array<string> ("3a", "213fd", "wffwafw"),
+  set_field (test_db, "attributes", "id",
+             array<string> ("33dsa", "213fd", "wf3fw"),
              (double) get_sec_time ());
 
   array<string> ans=
       get_attributes (test_db, "attributes", (double) get_sec_time ());
 
-  QVERIFY (ans == array<string> ("a1", "1b", "345asd"));
+  QVERIFY (ans == array<string> ("age", "name", "id"));
 }
 
 void
 TestDatabaseBasicFunciton::test_query () {
-  url test_db= url_system ("db6");
+  url    test_db= url_temp ("db6");
+  double t1     = (double) get_usec_time ();
 
-  set_field (test_db, "query1", "a", array<string> ("b", 1),
+  set_field (test_db, "query1", "no1", array<string> ("no1_name", "1"),
              (double) get_usec_time ());
-  set_field (test_db, "query1", "23", array<string> ("2b", 1),
+  set_field (test_db, "query1", "no2", array<string> ("no2_name", "2"),
              (double) get_usec_time ());
-  set_field (test_db, "query1", "11cd", array<string> ("3a", 1),
-             (double) get_usec_time ());
-
-  set_field (test_db, "query2", "a", array<string> ("bv", 1),
-             (double) get_usec_time ());
-  set_field (test_db, "query2", "23a", array<string> ("2b", 1),
-             (double) get_usec_time ());
-  set_field (test_db, "query2", "11cda", array<string> ("3a", 1),
+  set_field (test_db, "query1", "no3", array<string> ("no3_name", "3"),
              (double) get_usec_time ());
 
-  tree q1 (TUPLE, tree (TUPLE, "order", "a", "#t"));
-  auto a1= query (test_db, q1, (double) get_usec_time (), 1000000);
+  while ((double) get_usec_time () == t1)
+    ;
+  t1= (double) get_usec_time ();
 
+  set_field (test_db, "query2", "no1", array<string> ("no1_name", "1"),
+             (double) get_usec_time ());
+  set_field (test_db, "query2", "no4", array<string> ("no4_name", "4"),
+             (double) get_usec_time ());
+  set_field (test_db, "query2", "no5", array<string> ("no4_name", "4"),
+             (double) get_usec_time ());
+
+  // order to sort
   // sort ascending order
+  tree q1= tuple (tuple ("order", "\"no1_name\"", "#t"));
+  auto a1= query (test_db, q1, (double) get_usec_time (), 1000000);
+  // sort decending order
+  tree q2= tuple (tuple ("order", "\"no1_name\"", "#f"));
+  auto a2= query (test_db, q2, (double) get_usec_time (), 1000000);
+
   QVERIFY (a1 == array<string> ("query1", "query2"));
+  QVERIFY (a2 == array<string> ("query2", "query1"));
+
+  // modified: the modified id between t_begin and t_end
+  string t_begin (scm_quote (as_string (t1)));
+  string t_end (scm_quote (as_string ((double) get_usec_time ())));
+
+  tree q3= tuple (tuple ("modified", t_begin, t_end));
+  auto a3= query (test_db, q3, (double) get_usec_time (), 1000000);
+
+  string ans[1]= {"query2"};
+  QVERIFY (a3 == array<string> (ans, 1));
+
+  // test contains
+  tree q4= tuple (tuple ("contains", "\"no1_name\""));
+  auto a4= query (test_db, q4, (double) get_usec_time (), 1000000);
+  tree q5= tuple (tuple ("contains", "\"no2_name\""));
+  auto a5= query (test_db, q5, (double) get_usec_time (), 1000000);
+
+  QVERIFY (a4 == array<string> ("query1", "query2"));
+  string ans1[1]= {"query1"};
+  QVERIFY (a5 == array<string> (ans1, 1));
 }
 
 QTEST_MAIN (TestDatabaseBasicFunciton)
