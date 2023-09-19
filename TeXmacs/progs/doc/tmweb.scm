feb5b6b37549e76076fd939e938e1ca4ed3f0a0d
charonxin <pluto@liii.pro>
2023-09-19 09:24:37 +0800

    tidy debug code

--- TeXmacs/progs/doc/tmweb.scm
+++ TeXmacs/progs/doc/tmweb.scm
@@ -58,7 +58,6 @@
 
 (define (tmweb-convert-file tm-file html-file)
   (with-aux tm-file
-    (display* "[DEBUG] Converting :" (url->system tm-file) "\n")
     (if (url? html-file) (set! current-save-target html-file))
     (export-buffer-main (current-buffer) html-file "html" (list :overwrite))))
 
@@ -77,7 +76,6 @@
     (when (and (!= dir-name "CVS") (!= dir-name ".svn")
 	       (!= dir-name "prop-base") (!= dir-name "text-base"))
       (tmweb-make-dir dir (url-expand html-dir))
-      (display* "[DEBUG] needs-update? : " (needs-update? file u3 update?) "\n")
       (when (needs-update? file u3 update?)
         ;(system-wait "Converting" (url->system u1))
         (display* "TeXmacs] Converting " (url->system u1) "\n")
@@ -107,13 +105,11 @@
 	 (u3 (url-append u2 (url-wildcard "*.tm")))
 	 (u4 (url-expand (url-complete u3 "fr")))
 	 (u5 (url-expand (url-complete u1 "fr"))))
-   (display* "[DEBUG] url->list: " (url->list u4) " tm-dir: " tm-dir "\n")
     (when (!= html-dir tm-dir)
       (for-each (lambda (x) (tmweb-copy-file-dir x tm-dir html-dir update?))
                 (if keep? (url->list u5)
                     (list-difference (url->list u5) (url->list u4)))))
     (for-each (lambda (x) 
-      (display* "[DEBUG] the x: " x "\n")
       (tmweb-convert-file-dir x tm-dir html-dir update?))
 	      (url->list u4))))
 
