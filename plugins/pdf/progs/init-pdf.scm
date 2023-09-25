abc13ce29e58886450b695825fb05f9e54e97dfc
Tangdouer <884746853@qq.com>
2023-09-25 22:19:41 +0800

    license

--- plugins/pdf/progs/init-pdf.scm
+++ plugins/pdf/progs/init-pdf.scm
@@ -1,3 +1,14 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : init-pdf.scm
+;; DESCRIPTION : setup pdf converters
+;; COPYRIGHT   : (C) 2023  tandouer
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (texmacs-module (pdf-format))
 
 
