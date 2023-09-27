805c0bb8644fd485696905956732cc2d8c07477c
jingkaimori <jingkaimori@gmail.com>
2023-09-27 22:20:49 +0800

    bump mininum xmake version

--- xmake.lua
+++ xmake.lua
@@ -9,7 +9,7 @@
 -- It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
 -- in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 
-set_xmakever("2.8.2")
+set_xmakever("2.8.3")
 
 -- Check CXX Types/Includes/Funcs/Snippets
 includes("check_cxxtypes.lua")
