56a3d8307434c8e6764302ccb8fd7d06edfc3ebd
Da Shen <da@liii.pro>
2023-10-01 22:38:29 +0800

    wip

--- TeXmacs/progs/utils/misc/translation-list.scm
+++ TeXmacs/progs/utils/misc/translation-list.scm
@@ -174,10 +174,12 @@
 (define (tr-file language)
   (let* ((locale_tag (language-to-locale language))
          (lang_plugin_dir (string-append "plugins/lang_" locale_tag "/progs/"))
-         (lang_dic_file (string-append "english-" language ".scm")))
-   (url-or (url-append (url-append (get-texmacs-home-path) lang_plugin_dir) lang_dic_file)
-    (url-or (url-append (url-append (get-texmacs-path) lang_plugin_dir) lang_dic_file)
-            (system->url (string-append "$TEXMACS_PATH/langs/natural/dic/" lang_dic_file))))))
+         (lang_dic_file (string-append "english-" language ".scm"))
+         (candidates
+          (url-or (url-append (url-append (get-texmacs-home-path) lang_plugin_dir) lang_dic_file)
+           (url-or (url-append (url-append (get-texmacs-path) lang_plugin_dir) lang_dic_file)
+                   (system->url (string-append "$TEXMACS_PATH/langs/natural/dic/" lang_dic_file))))))
+   (url-resolve candidates "r")))
 
 (define (tr-miss language)
   (url-concretize
