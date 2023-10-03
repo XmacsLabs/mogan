9698ed57098a9d82209d31e8c49f43046da2e463
charonxin <pluto@liii.pro>
2023-10-03 23:18:53 +0800

    [57_2] Remove ispell from libmogan

--- xmake.lua
+++ xmake.lua
@@ -291,7 +291,7 @@ set_configvar("PDFHUMMUS_NO_TIFF", true)
 set_configvar("USE_PLUGIN_BIBTEX", true)
 set_configvar("USE_PLUGIN_LATEX_PREVIEW", true)
 set_configvar("USE_PLUGIN_TEX", true)
-set_configvar("USE_PLUGIN_ISPELL", true)
+set_configvar("USE_PLUGIN_ISPELL", false)
 set_configvar("TM_DYNAMIC_LINKING", false)
 
 if is_plat("mingw", "windows") then
@@ -384,6 +384,21 @@ libmogan_headers = {
     "src/Kernel/Abstractions",
     "src/Kernel/Types",
     "src/Plugins",
+    -- "src/Plugins/Bibtex",
+    -- "src/Plugins/Cocoa",
+    -- "src/Plugins/Database",
+    -- "src/Plugins/Freetype",
+    -- "src/Plugins/Ghostscript",
+    -- -- "src/Plugins/Ispell",
+    -- "src/Plugins/LaTeX_Preview",
+    -- "src/Plugins/MacOS",
+    -- "src/Plugins/Metafont",
+    -- "src/Plugins/Openssl",
+    -- "src/Plugins/Pdf",
+    -- "src/Plugins/Qt",
+    -- "src/Plugins/Tex",
+    -- "src/Plugins/Updater",
+    -- "src/Plugins/Xml",
     "src/Scheme",
     "src/Scheme/S7",
     "src/Scheme/L1",
@@ -500,7 +515,7 @@ target("libmogan") do
     add_files(plugin_freetype_srcs)
     add_files(plugin_database_srcs)
     add_files(plugin_ghostscript_srcs)
-    add_files(plugin_ispell_srcs)
+    -- add_files(plugin_ispell_srcs)
     add_files(plugin_metafont_srcs)
     add_files(plugin_tex_srcs)
     add_files(plugin_latex_preview_srcs)
