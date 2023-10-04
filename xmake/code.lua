45ed19e3e643103db3f2ee0cebb8f4e18e696ef9
Da Shen <da@liii.pro>
2023-10-04 10:56:40 +0800

    copyright

--- xmake/code.lua
+++ xmake/code.lua
@@ -1,3 +1,13 @@
+-------------------------------------------------------------------------------
+--
+-- MODULE      : code.lua
+-- DESCRIPTION : Xmake config file for Mogan Code
+-- COPYRIGHT   : (C) 2022-2023  Darcy Shen
+--
+-- This software falls under the GNU general public license version 3 or later.
+-- It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+-- in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+
 function add_target_code()
     set_languages("c++17")
     set_version(XMACS_VERSION, {build = "%Y-%m-%d"})
