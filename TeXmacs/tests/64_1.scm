84c2a777af0d97b4c8644b2632aed1e9843811b4
Yiheng Wu <36156959+jingkaimori@users.noreply.github.com>
2023-10-06 16:34:39 +0800

    [64_1] fix for de-embedding of images with unicode filename
    ## Why you open this Pull Request?
    
    fix cork encoding of filename in "save image as..." dialog
    
    ## What work have you done in the current Pull Request?
    
    - [x] fix
    
    ---------
    
    Co-authored-by: Philippe Joyez <coding.e.pjoyez@neverbox.com>

---
+++ TeXmacs/tests/64_1.scm
@@ -0,0 +1,37 @@
+(define (file-under-path path)
+  (lambda (file)
+    (url-concretize (url-append path file))))
+
+(define embedded-propose-4th
+  (lambda (tree)
+    (embedded-propose tree 4)))
+
+(define (test-ascii-filename)
+  (load-buffer "$TEXMACS_PATH/tests/64_1.tm")
+  (let ((image-ascii (tree-ref (root-tree) 1 1))
+        (image-untitled (tree-ref (root-tree) 1 2))
+        (image-cork (tree-ref (root-tree) 1 3)))
+    (regression-test-group
+      "embedded-propose on latin filename" "embedded-propose-latin"
+      embedded-propose-4th (file-under-path "$TEXMACS_PATH/tests")
+      (test "cork encoding" image-cork "学生：张佳.png")
+      (test "ascii"  image-ascii "64_1.png")
+      (test "extension only" image-untitled "64_1-image-4.png"))))
+
+(define (test-cjk-filename)
+  (load-buffer "$TEXMACS_PATH/tests/64_1_中文文件名.tm")
+  (let ((image-ascii (tree-ref (root-tree) 1 1))
+        (image-untitled (tree-ref (root-tree) 1 2))
+        (image-cork (tree-ref (root-tree) 1 3)))
+    (regression-test-group
+      "embedded-propose on unicode filename" "embedded-propose-unicode"
+      embedded-propose-4th (file-under-path "$TEXMACS_PATH/tests")
+      (test "cork encoding" image-cork "学生：张佳.png")
+      (test "ascii"  image-ascii "64_1.png")
+      (test "extension only" image-untitled "64_1_中文文件名-image-4.png"))))
+
+(tm-define (test_64_1)
+  (let ((n (+ (test-ascii-filename)
+              (test-cjk-filename))))
+    (display* "Total: " (object->string n) " tests.\n")
+    (display "Test suite of 64_1: ok\n")))
