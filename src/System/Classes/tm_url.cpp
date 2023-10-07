34c20ce703dab33d332bcacc32d593a3e411deb9
Darcy Shen <da@liii.pro>
2023-10-07 17:11:44 +0800

    [53_13] Fix for the tmfs_url removal from url_system

--- src/System/Classes/tm_url.cpp
+++ src/System/Classes/tm_url.cpp
@@ -70,6 +70,10 @@
 #include "analyze.hpp"
 #include "scheme.hpp"
 
+url tm_url (string name) {
+  if (starts (name, "tmfs://")) return tmfs_url (name (7, N (name)));
+  else return url_general (name, URL_UNIX);
+}
 
 bool url_test (url name, string filter) {
     if (filter == "") return true;
--- src/Texmacs/Data/new_view.cpp
+++ src/Texmacs/Data/new_view.cpp
@@ -18,7 +18,7 @@
 #include "dictionary.hpp"
 #include "new_document.hpp"
 #include "drd_std.hpp"
-#include "tmfs_url.hpp"
+#include "tm_url.hpp"
 
 #if defined(OS_MINGW) || defined(OS_WIN)
 #define WINPATHS
@@ -72,7 +72,7 @@ abstract_view (tm_view vw) {
   string name= encode_url (vw->buf->buf->name);
   //cout << vw->buf->buf->name << " -> " << name << "\n";
   string nr  = as_string (vw->nr);
-  return tmfs_url ("/view/" * nr * "/" * name);
+  return tm_url ("tmfs://view/" * nr * "/" * name);
 }
 
 tm_view
