ce16bfd93602aa09b76a45b4145f8b5449f031b4
Darcy Shen <da@liii.pro>
2023-10-19 10:05:17 +0800

    minor improve

--- TeXmacs/progs/graphics/graphics-markup.scm
+++ TeXmacs/progs/graphics/graphics-markup.scm
@@ -105,6 +105,7 @@
                     (points-add (point-times (point-get-unit (points-sub mid-p-x c)) r) c))))))
     `(arc ,p ,m ,x)))
 
+
 (define-graphics (sector C P Q)
   (let* ((c  (if (tm-point? C) (tree->stree C) '(point "0" "0")))
          (p  (if (tm-point? P) (tree->stree P) c))
@@ -117,23 +118,22 @@
          (vec-c-p (points-sub p c))
          (vec-c-q (points-sub q c))
          (m  (if (equal? r 0.0)
-                c
-                (if (> (points-cross-product-k vec-c-p vec-c-q) 0)
-                  (points-add (point-times (point-get-unit (points-sub mid-p-x c)) (- r)) c)
-                  (if (= (points-cross-product-k vec-c-p vec-c-q) 0)
-                    x
-                    (points-add (point-times (point-get-unit (points-sub mid-p-x c)) r) c))))))
+                 c
+                 (if (> (points-cross-product-k vec-c-p vec-c-q) 0)
+                     (points-add (point-times (point-get-unit (points-sub mid-p-x c)) (- r)) c)
+                     (if (= (points-cross-product-k vec-c-p vec-c-q) 0)
+                         x
+                         (points-add (point-times (point-get-unit (points-sub mid-p-x c)) r) c))))))
        (if (> (points-cross-product-k vec-c-p vec-c-q) 0)
         `(superpose
           (std-arc ,c ,p ,m)
           (std-arc ,c ,m ,x)
           (with "color" "none"
-          (line ,p ,c ,x ,m ,p)))
+           (line ,p ,c ,x ,m ,p)))
         `(superpose    
-        (with "color" "none" 
-          (line ,p ,c ,x))
-        (arc ,p ,m ,x)))))
-
+          (with "color" "none"
+           (line ,p ,c ,x))
+          (arc ,p ,m ,x)))))
 
 
 (define-graphics (three-points-circle P1 P2 P3)
@@ -142,11 +142,13 @@
          (p3 (if (tm-point? P3) P3 p2)))
     `(carc ,p1 ,p2 ,p3)))
 
+
 (define-graphics (three-points-arc P1 P2 P3)
   (let* ((p1 (if (tm-point? P1) P1 '(point "0" "0")))
          (p2 (if (tm-point? P2) P2 p1))
          (p3 (if (tm-point? P3) P3 p2)))
     `(arc ,p1 ,p2 ,p3)))
+
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Electrical diagrams
