1b0bfef1af3bc5a6065204566fbf993bf9b47d51
Darcy Shen <da@liii.pro>
2023-10-19 15:45:27 +0800

    wip

--- xmake.lua
+++ xmake.lua
@@ -571,131 +571,30 @@ target("windows_installer") do
     end)
 end
 
-function add_test_target (filepath, dep)
-    local testname = path.basename(filepath)
-    target(testname) do
-        set_enabled(not is_plat("wasm"))
-        add_runenvs("TEXMACS_PATH", path.join(os.projectdir(), "TeXmacs"))
-        set_group("tests")
-        add_deps(dep)
-        set_languages("c++17")
-        set_policy("check.auto_ignore_flags", false)
-        if is_plat("windows") then
-            set_encodings("utf-8") -- eliminate warning C4819 on msvc
-            add_ldflags("/LTCG")
-        end
-        if is_plat("windows", "mingw") then
-            add_syslinks("secur32")
-        end
-        add_rules("qt.console")
-        add_frameworks("QtGui", "QtWidgets", "QtCore", "QtPrintSupport", "QtSvg", "QtTest")
-        if not is_plat("windows") then
-            add_syslinks("pthread")
-        end
-        if is_plat ("mingw") then
-            add_packages("mingw-w64")
-        end
-        add_packages("s7")
-        add_packages("lolly")
 
-        add_includedirs({"$(buildir)", "tests/Base"})
-        add_includedirs(libmogan_headers)
-        build_glue_on_config()
-        add_files("tests/Base/base.cpp")
-        add_files(filepath)
-        add_files(filepath, {rules = "qt.moc"})
-        before_build(function (target)
-            target:add("forceincludes", path.absolute("$(buildir)/config.h"))
-        end)
-
-        if is_plat("wasm") then
-            on_run(function (target)
-                node = os.getenv("EMSDK_NODE")
-                cmd = node .. " $(buildir)/wasm/wasm32/$(mode)/" .. testname .. ".js"
-                print("> " .. cmd)
-                os.exec(cmd)
-            end)
-        end
-    end
-end
-
+includes("xmake/tests.lua")
 -- Tests in C++
 l3_cpp_tests = os.files("tests/L3/**_test.cpp")
 all_cpp_tests = os.files("tests/**_test.cpp")
 
 for _, filepath in ipairs(l3_cpp_tests) do
-    add_test_target (filepath, "libkernel_l3")
+    add_target_cpp_test(filepath, "libkernel_l3")
 end
 
 for _, filepath in ipairs(all_cpp_tests) do
     if not table.contains(l3_cpp_tests, filepath) then
-        add_test_target (filepath, "libmogan")
+        add_target_cpp_test(filepath, "libmogan")
     end
 end
 
+-- Tests in Scheme
 for _, filepath in ipairs(os.files("TeXmacs/progs/**/*-test.scm")) do
-    local testname = path.basename(filepath)
-    target(testname) do
-        set_enabled(not is_plat("wasm"))
-        set_kind("phony")
-        set_group("scheme_tests")
-        on_run(function (target)
-            name = target:name()
-            regtest_name = "(regtest-"..string.sub(name, 1, -6)..")"
-            print("Executing: " .. regtest_name)
-            params = {
-                "-headless",
-                "-b", filepath,
-                "-x", regtest_name,
-                "-q"
-            }
-            if is_plat("macosx", "linux") then
-                binary = target:deps()["research"]:targetfile()
-            elseif is_plat("mingw", "windows") then
-                binary = path.join(INSTALL_DIR,"bin","MGResearch.exe")
-            else
-                print("Unsupported plat $(plat)")
-            end
-            cmd = binary
-            if is_plat("macosx", "linux") then
-                os.execv(cmd, params, {envs=RUN_ENVS})
-            else
-                os.execv(cmd, params)
-            end
-        end)
-    end
+    add_target_scheme_test(filepath)
 end
 
+-- Integration tests
 for _, filepath in ipairs(os.files("TeXmacs/tests/**/*.scm")) do
-    local testname = path.basename(filepath)
-    target(testname) do
-        set_enabled(not is_plat("wasm"))
-        set_kind("phony")
-        set_group("integration_tests")
-        add_deps("research")
-        on_run(function (target)
-            name = target:name()
-            params = {
-                "-headless",
-                "-b", path.join("TeXmacs","tests",name..".scm"),
-                "-x", "(test_"..name..")",
-                "-q"
-            }
-            if is_plat("macosx", "linux") then
-                binary = target:deps()["research"]:targetfile()
-            elseif is_plat("mingw", "windows") then
-                binary = path.join(INSTALL_DIR,"bin","MGResearch.exe")
-            else
-                print("Unsupported plat $(plat)")
-            end
-            cmd = binary
-            if is_plat("macosx", "linux") then
-                os.execv(cmd, params, {envs=RUN_ENVS})
-            else
-                os.execv(cmd, params)
-            end
-        end)
-    end
+    add_target_integration_test(filepath)
 end
 
 
