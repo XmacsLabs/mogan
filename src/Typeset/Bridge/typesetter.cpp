5f8724a21742dcc1fc87255bc88ac2ba44e73744
Darcy Shen <da@liii.pro>
2023-10-23 20:12:11 +0800

    [7_2] Use long array instead of long list to avoid stack overflow

--- src/Typeset/Bridge/typesetter.cpp
+++ src/Typeset/Bridge/typesetter.cpp
@@ -105,16 +105,15 @@ typesetter_rep::local_end (array<page_item>& prev_l, stack_border& prev_sb) {
 * Main typesetting routines
 ******************************************************************************/
 
-static rectangles
-requires_update (rectangles log) {
-  rectangles rs;
-  while (!is_nil (log)) {
-    rectangle r1= log->item;
-    rectangle r2= log->next->item;
-    if (r1 == rectangle (0, 0, 0, 0)) rs= rectangles (r2, rs);
-    else if (r2 == rectangle (0, 0, 0, 0)) rs= rectangles (r1, rs);
-    else if (r1 != r2) rs= rectangles (r1, rectangles (r2, rs));
-    log= log->next->next;
+static array<rectangle>
+requires_update (array<rectangle> log) {
+  array<rectangle> rs;
+  for (int i=0; 2*i+1<N(log); i++) {
+    rectangle r1= log[2*i];
+    rectangle r2= log[2*i+1];
+    if (r1 == rectangle (0, 0, 0, 0)) rs << r2;
+    else if (r2 == rectangle (0, 0, 0, 0)) rs << r1;
+    else if (r1 != r2) rs << r1 << r2;
   }
   return reverse (rs);
 }
@@ -181,7 +180,7 @@ typesetter_rep::typeset (SI& x1b, SI& y1b, SI& x2b, SI& y2b) {
   b->position_at (0, 0, change_log);
   change_log= requires_update (change_log);
   rectangle r (0, 0, 0, 0);
-  if (!is_nil (change_log)) r= least_upper_bound (change_log);
+  if (N(change_log) != 0) r= least_upper_bound (change_log);
   array<brush> new_bgs;
   array<rectangle> rs;
   b->collect_page_colors (new_bgs, rs);
@@ -190,7 +189,7 @@ typesetter_rep::typeset (SI& x1b, SI& y1b, SI& x2b, SI& y2b) {
       r= least_upper_bound (r, rs[i]);
   old_bgs= new_bgs;
   x1b= r->x1; y1b= r->y1; x2b= r->x2; y2b= r->y2;
-  change_log= rectangles ();
+  change_log= array<rectangle> ();
   return b;
 }
 
