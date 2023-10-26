0d5109d269668d3564682e4cd28411dd05f2e69a
Darcy Shen <da@liii.pro>
2023-10-26 15:53:11 +0800

    fix

--- TeXmacs/progs/texmacs/menus/preferences-widgets.scm
+++ TeXmacs/progs/texmacs/menus/preferences-widgets.scm
@@ -574,6 +574,7 @@
     (meti (hlist // (text "Expand beamer slides"))
       (toggle (set-boolean-preference "texmacs->pdf:expand slides" answer)
               (get-boolean-preference "texmacs->pdf:expand slides")))
+    (assuming (supports-native-pdf?)
       (meti (hlist // (text "Check exported Pdf files for correctness"))
         (toggle (set-boolean-preference "texmacs->pdf:check" answer)
                 (get-boolean-preference "texmacs->pdf:check")))))
