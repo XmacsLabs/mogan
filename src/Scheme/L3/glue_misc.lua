e6b72a2af08331c17bc6ade2bdd4ebf5c210b1ae
Darcy Shen <da@liii.pro>
2023-10-31 18:04:52 +0800

    [51_6] Scheme Glue: xmacs-version

--- src/Scheme/L3/glue_misc.lua
+++ src/Scheme/L3/glue_misc.lua
@@ -123,6 +123,11 @@ function main()
                 arg_list = {
                     "url"
                 }
+            },
+            {
+                scm_name = "xmacs-version",
+                cpp_name = "xmacs_version",
+                ret_type = "string"
             }
         }
     }
