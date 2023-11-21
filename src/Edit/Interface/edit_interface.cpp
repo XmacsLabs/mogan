02babdb84e3ab5a118fd69b481fcc7199dc31d3e
Darcy Shen <da@liii.pro>
2023-11-21 15:32:53 +0800

    [7_4] Fix diff freeze when using the Rough grain
    Co-authored-by: 沈浪熊猫儿 <shenda@ustc.edu>

--- src/Edit/Interface/edit_interface.cpp
+++ src/Edit/Interface/edit_interface.cpp
@@ -512,6 +512,7 @@ correct_adjacent (rectangles& rs1, rectangles& rs2) {
 void
 edit_interface_rep::compute_env_rects (path p, rectangles& rs, bool recurse) {
   if (p == rp) return;
+  if (is_nil (p)) return;
   tree pt= subtree (et, path_up (p));
   tree st= subtree (et, p);
   if ((is_func (st, TABLE) || is_func (st, SUBTABLE)) &&
