d953bb7d7c18cdcd91afb8c9a8122453011a535a
Darcy Shen <da@liii.pro>
2023-12-03 20:07:00 +0800

    [39_7] Initial welcome doc for Mogan Research

---
+++ TeXmacs/doc/about/mogan/research.en.tm
@@ -0,0 +1,21 @@
+<TeXmacs|2.1.2>
+
+<style|<tuple|tmdoc|chinese>>
+
+<\body>
+  <tmdoc-title|Mogan Research v<Xmacs-version>>
+
+  <with|framed-color|pastel magenta|ornament-border|0ln|ornament-color|pastel
+  yellow|<\ornamented>
+    The latest stable release is <strong|v<include|http://git.tmml.wiki/XmacsLabs/planet/raw/branch/main/latest_version.tm>>.
+  </ornamented>>
+
+  \;
+
+  \;
+
+  \;
+</body>
+
+<initial|<\collection>
+</collection>>
--- TeXmacs/packages/standard/std-markup.ts
+++ TeXmacs/packages/standard/std-markup.ts
@@ -30,6 +30,8 @@
 
   <assign|TeXmacs-version|<macro|<extern|texmacs-version>>>
 
+  <assign|Xmacs-version|<macro|<extern|xmacs-version>>>
+
   <assign|TeXmacs-version-release|<macro|type|<extern|texmacs-version-release*|<quote-arg|type>>>>
 
   <assign|made-by-TeXmacs|<macro|<float|footnote||<with|font-size|0.84|par-mode|justify|par-left|0cm|par-right|0cm|<active*|<move|<image|local:$TEXMACS_PATH/misc/images/tm_gnu3.ps||1fn||>|0fn|-0.2fn>><space|2spc><localize|This
