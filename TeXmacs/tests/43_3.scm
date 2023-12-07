81874f63a73a1abe5a501c054e15afaf87aae353
Darcy Shen <da@liii.pro>
2023-12-07 15:30:31 +0800

    wip

--- TeXmacs/tests/43_3.scm
+++ TeXmacs/tests/43_3.scm
@@ -1,20 +1,22 @@
 (define (export-as-latex-and-load path)
-  (with tmpfile (url-temp)
-    (load-buffer path)
-    (buffer-export path tmpfile "latex")
-    (string-load tmpfile)))
+  (with path (string-append "$TEXMACS_PATH/tests/" path)
+    (with tmpfile (url-temp)
+      (load-buffer path)
+      (buffer-export path tmpfile "latex")
+      (string-load tmpfile))))
+  
+(define (load-latex path)
+  (with path (string-append "$TEXMACS_PATH/tests/" path)
+    (string-load path)))
 
-(define (test-export-empty-doc-as-latex)
+(define (test-export-as-latex)
   (regression-test-group
    "export to latex and load as string" "load as string"
-   export-as-latex-and-load string-load
-   (test "tm file with only a" "$TEXMACS_PATH/tests/43_3_a.tm" "$TEXMACS_PATH/tests/43_3_a.tex")))
-
-(define (test-export-a-doc-as-latex)
-        1)
+   export-as-latex-and-load load-latex
+   (test "tm file with only a" "43_3_a.tm" "43_3_a.tex")
+   (test "test empty tm file" "43_3_empty.tm" "43_3_empty.tex")))
 
 (tm-define (test_43_3)
-  (let ((n (+ (test-export-empty-doc-as-latex)
-              (test-export-a-doc-as-latex))))
+  (let ((n (+ (test-export-as-latex))))
     (display* "Total: " (object->string n) " tests.\n")
     (display "Test suite of 9_1: ok\n")))
