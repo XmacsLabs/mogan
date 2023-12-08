1f6ad29de441ab3070c23c7772456799848150bf
Darcy Shen <da@liii.pro>
2023-12-08 14:59:48 +0800

    [22_12] Dic_zh: tooltips for the Tool menu

--- TeXmacs/progs/texmacs/texmacs/tm-tools.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-tools.scm
@@ -67,7 +67,7 @@
   (set-show-kbd (not (get-show-kbd))))
 
 (tm-define (clear-font-cache)
-  (:synopsis "Clear font cache under TEXMACS_HOME_PATH.")
+  (:synopsis "Clear font cache under TEXMACS_HOME_PATH and local cache path.")
   (system-remove (url-append (get-tm-cache-path) (string->url "font_cache.scm")))
   (map system-remove
     (list
