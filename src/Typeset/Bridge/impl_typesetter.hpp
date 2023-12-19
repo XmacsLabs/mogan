48271472730ecc86eeb4720a81e089162d4dc472
Darcy Shen <da@liii.pro>
2023-12-19 18:46:17 +0800

    [7_2] Revert: Use long array instead of long list to avoid stackoverflow
    This reverts commit 5f8724a21742dcc1fc87255bc88ac2ba44e73744.

--- src/Typeset/Bridge/impl_typesetter.hpp
+++ src/Typeset/Bridge/impl_typesetter.hpp
@@ -17,7 +17,7 @@ class typesetter_rep {
 public:
   edit_env&    env;
   bridge       br;
-  array<rectangle> change_log;
+  rectangles   change_log;
   array<brush> old_bgs;
 
   array<page_item> l;      // current lines
