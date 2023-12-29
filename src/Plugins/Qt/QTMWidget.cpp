4d1ecb3a16599239e01b9edfc54c769b39457da0
Darcy Shen <da@liii.pro>
2023-12-29 17:55:41 +0800

    wip

--- src/Plugins/Qt/QTMWidget.cpp
+++ src/Plugins/Qt/QTMWidget.cpp
@@ -315,7 +315,7 @@ QTMWidget::keyPressEvent (QKeyEvent* event) {
   // else {
   //   cout << "mods:\t" << mods_text << LF;
   // }
-  // cout << "text(" << nss.count () << "):\t" << from_qstring (nss) << LF;
+  // cout << "text(" << nss.count () << "):\t" << from_qstring_utf8 (nss) << LF;
   // cout << LF;
 
   string r= "";
@@ -377,16 +377,18 @@ QTMWidget::keyPressEvent (QKeyEvent* event) {
   }
   else {
     initkeymap ();
-    if (qtkeymap->contains (key)) {
-      r= qtkeymap[key];
+    if (qtkeymap->contains (key)) { // Case 1: in qtkeymap
+      r= mods_text * qtkeymap[key];
     }
-    else if (qtdeadmap->contains (key)) {
+    else if (qtdeadmap->contains (key)) { // Case 2: in qtdeadmap
       mods&= ~Qt::ShiftModifier;
-      r= qtdeadmap[key];
+      r= mods_text * qtdeadmap[key];
     }
-    else {
+    else if (key >= 256) { // Case 3: Unicode >= 256
+      r= from_qstring_utf8 (nss);
+    }
+    else { // Case 4: Unicode < 256
       // We need to use text(): Alt-{5,6,7,8,9} are []|{} under MacOS, etc.
-      unsigned int   kc  = event->nativeVirtualKey ();
       unsigned short unic= nss.data ()[0].unicode ();
       if (unic > 32 && unic < 255 && (mods & Qt::ShiftModifier) != 0 &&
           (mods & Qt::ControlModifier) == 0 && (mods & Qt::AltModifier) == 0 &&
@@ -435,9 +437,7 @@ QTMWidget::keyPressEvent (QKeyEvent* event) {
             r= "tilde";
             break;
           default:
-            QByteArray buf= nss.toUtf8 ();
-            string     rr (buf.constData (), buf.size ());
-            r= rr;
+            r= from_qstring_utf8 (nss);
           }
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
           if (os_macos () && (mods & Qt::AltModifier)) {
@@ -454,10 +454,11 @@ QTMWidget::keyPressEvent (QKeyEvent* event) {
             else mods&= ~Qt::AltModifier; // unset Alt
           }
 #endif
+          if (!is_empty (r)) {
+            r= mods_text * r;
+          }
         }
-    }
-    if (is_empty (r)) return;
-    r= mods_text * r;
+    } // Case 4: Unicode < 256 (endif)
   }
 
   if (is_empty (r)) return;
