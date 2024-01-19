aadbad7352209f7309bd7058e0a98567a2f12e4e
Darcy Shen <da@liii.pro>
2024-01-20 01:26:09 +0800

    add license headers

--- xmake/3rd/mimalloc.lua
+++ xmake/3rd/mimalloc.lua
@@ -1,3 +1,23 @@
+--! package mimalloc
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2020-present, TBOOX Open Source Group.
+--
+-- @author      maximegmd, yecate, waruqi, Yamashi, Force67, heheda123123, Darcy Shen
+-- @file        mimalloc.lua
+--
+
 package("mimalloc")
 
     set_homepage("https://github.com/microsoft/mimalloc")
--- xmake/3rd/zlib.lua
+++ xmake/3rd/zlib.lua
@@ -1,3 +1,23 @@
+--! package zlib
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+--
+-- Copyright (C) 2018-present, TBOOX Open Source Group.
+--
+-- @author      waruqi, xq114, a952135763, PucklaJ, ImperatorS79, Darcy Shen
+-- @file        zlib.lua
+--
+
 package("zlib")
     set_homepage("http://www.zlib.net")
     set_description("A Massively Spiffy Yet Delicately Unobtrusive Compression Library")
