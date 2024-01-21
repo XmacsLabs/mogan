612f01a5032e7b169d65d6077f72a2f7f0c6c863
Darcy Shen <da@liii.pro>
2024-01-21 11:05:35 +0800

    wip

--- xmake/packages.lua
+++ xmake/packages.lua
@@ -42,7 +42,7 @@ includes ("3rd/l/lolly/xmake.lua")
 includes ("3rd/p/pdfhummus/xmake.lua")
 includes ("3rd/s/s7/xmake.lua")
 includes ("3rd/z/zlib/xmake.lua")
-add_repositories("benchbox-repo xmake/3rd")
+add_repositories("mogan-repo xmake")
 
 function add_requires_of_mogan()
     -- package: s7
