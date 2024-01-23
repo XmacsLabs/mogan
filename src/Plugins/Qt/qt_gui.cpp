c1da82122b96a0197a2d8aed9ef272a3bbf86978
Darcy Shen <da@liii.pro>
2024-01-23 16:16:53 +0800

    [71_31] Fix crash when launching in the second monitor on macOS

--- src/Plugins/Qt/qt_gui.cpp
+++ src/Plugins/Qt/qt_gui.cpp
@@ -204,6 +204,9 @@ qt_gui_rep::qt_gui_rep (int& argc, char** argv)
     retina_scale= as_double (get_user_preference ("retina-scale"));
 
   qApp->setAttribute (Qt::AA_UseHighDpiPixmaps);
+  if (!use_native_menubar) {
+    qApp->setAttribute (Qt::AA_DontUseNativeMenuBar);
+  }
 }
 
 /* important routines */
