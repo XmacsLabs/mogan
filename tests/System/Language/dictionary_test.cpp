ea7f462df2b94cfa2c9066e8913f0e9e82c5b506
jingkaimori <jingkaimori@gmail.com>
2024-01-30 15:05:49 +0800

    add test for string translation

---
+++ tests/System/Language/dictionary_test.cpp
@@ -0,0 +1,51 @@
+
+/******************************************************************************
+ * MODULE     : dictionary_test.cpp
+ * COPYRIGHT  : (C) 2019  Darcy Shen
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
+#include "base.hpp"
+#include "converter.hpp"
+#include "dictionary.hpp"
+#include <QtTest/QtTest>
+
+class TestDictionary : public QObject {
+  Q_OBJECT
+
+private slots:
+  void init () { init_lolly (); }
+  void test_svg_image_size ();
+  void test_image_size ();
+};
+
+void
+TestDictionary::test_svg_image_size () {
+  qcompare (translate (string ("yes"), "english", "chinese"),
+            utf8_to_cork ("是"));
+  qcompare (translate (string ("!yes??"), "english", "chinese"),
+            utf8_to_cork ("!是??"));
+
+  set_output_language ("chinese");
+  qcompare (get_output_language (), "chinese");
+  qcompare (translate ("yes"), utf8_to_cork ("是"));
+  qcompare (translate (string ("yes")), utf8_to_cork ("是"));
+
+  qcompare (translate (string ("!yes??")), utf8_to_cork ("!是??"));
+  qcompare (translate_as_is (string ("!yes??")), utf8_to_cork ("!yes??"));
+  qcompare (translate (string ("yes::disambig")), utf8_to_cork ("是"));
+  qcompare (translate_as_is (string ("yes::disambig")), utf8_to_cork ("是"));
+  qcompare (translate (string ("translate")), utf8_to_cork ("是"));
+  qcompare (translate (string ("translate::language")), utf8_to_cork ("是"));
+}
+
+void
+TestDictionary::test_image_size () {
+
+}
+
+QTEST_MAIN (TestDictionary)
+#include "dictionary_test.moc"
