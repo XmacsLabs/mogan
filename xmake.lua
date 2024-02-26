3ec0fe9d7c8469802592cc57f9a2dc3f6ed14c9f
Darcy Shen <da@liii.pro>
2024-02-26 20:25:58 +0800

    [1.2.6] bump to 1.2.6-alpha

--- xmake.lua
+++ xmake.lua
@@ -27,12 +27,12 @@ configvar_check_cxxincludes("HAVE_UTIL_H", "util.h")
 set_project("Mogan STEM Suite")
 
 -- only lock it in rc releases
-if is_plat ("macosx", "windows") then
-    set_policy("package.requires_lock", true)
-end
+-- if is_plat ("macosx", "windows") then
+--     set_policy("package.requires_lock", true)
+-- end
 
 local TEXMACS_VERSION = "2.1.2"
-local XMACS_VERSION="1.2.5"
+local XMACS_VERSION="1.2.6-alpha"
 local CONFIG_USER = "XmacsLabs"
 local DEVEL_VERSION = TEXMACS_VERSION
 local DEVEL_RELEASE = 1
