4427dc787577341284c51910578c0421e92d8781
Darcy Shen <da@liii.pro>
2024-03-04 14:03:19 +0800

    wip

--- TeXmacs/progs/app/beamer/buffer.scm
+++ TeXmacs/progs/app/beamer/buffer.scm
@@ -1,3 +1,17 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : buffer.scm
+;; DESCRIPTION : buffer handling
+;; COPYRIGHT   : (C) 2001-2021  Joris van der Hoeven
+;;                   2024       Darcy Shen
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (texmacs-module (app beamer buffer))
 
 (tm-define (buffer-set-default-style)
