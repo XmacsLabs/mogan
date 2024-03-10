4ca2c88f61d3bf8b46184450fb8d8772e11a2158
Darcy Shen <da@liii.pro>
2024-03-10 20:33:59 +0800

    wip

--- xmake/tests.lua
+++ xmake/tests.lua
@@ -79,6 +79,7 @@ function add_target_cpp_bench(filepath, dep)
         end
         add_packages("s7")
         add_packages("lolly")
+        add_packages("moebius")
         add_packages("pdfhummus")
 
         add_includedirs(libmogan_headers)
