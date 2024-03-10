cb80d18db63918fbd0d0a4369ebb6c2623ce17af
Darcy Shen <da@liii.pro>
2024-03-10 20:20:16 +0800

    wip

--- xmake/packages/m/moebius/xmake.lua
+++ xmake/packages/m/moebius/xmake.lua
@@ -24,7 +24,7 @@ package("moebius")
 
     add_urls("https://github.com/XmacsLabs/moebius.git")
     add_urls("https://gitee.com/XmacsLabs/moebius.git")
-    add_versions("0.1.5", "v0.1.5")
+    add_versions("0.1.6", "v0.1.6")
 
     add_deps("lolly")
 
