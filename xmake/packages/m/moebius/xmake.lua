ec3b8905a58175ccf3bd17983d79cb487a481494
Darcy Shen <da@liii.pro>
2024-03-12 05:55:59 +0800

    [73_19] moebius 0.1.13

--- xmake/packages/m/moebius/xmake.lua
+++ xmake/packages/m/moebius/xmake.lua
@@ -24,7 +24,7 @@ package("moebius")
 
     add_urls("https://github.com/XmacsLabs/moebius.git")
     add_urls("https://gitee.com/XmacsLabs/moebius.git")
-    add_versions("0.1.12", "v0.1.12")
+    add_versions("0.1.13", "v0.1.13")
 
     add_deps("lolly")
 
