117e76cf7963c2c32cb59236cc2faae73f168a92
Darcy Shen <da@liii.pro>
2024-04-02 21:48:14 +0800

    [61_2] Python plugin v0.0.2
    ## What
    Include Python plugin v0.0.2 as the built-in plugin.
    
    Include
    https://github.com/texmacs/plugins-in-python/releases/download/python_v0.0.2/python_any_v0.0.2.tar.gz
    with the README.md removed.
    
    ## Why
    Python is a popular and powerful programming language.
    
    ## How to test your changes?
    Click `Help->Plugins->Python` and eval the python session.

---
+++ TeXmacs/plugins/python/progs/init-python.scm
@@ -0,0 +1,96 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : init-python.scm
+;; DESCRIPTION : Initialize python plugin
+;; COPYRIGHT   : (C) 2004  Ero Carrera,
+;;               (C) 2012  Adrian Soto
+;;               (C) 2020  Darcy Shen
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-modules
+  (dynamic session-edit)
+  (dynamic program-edit)
+  (binary python3))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Python source files
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-format python
+  (:name "Python Source Code")
+  (:suffix "py"))
+
+(define (texmacs->python x . opts)
+  (texmacs->verbatim x (acons "texmacs->verbatim:encoding" "SourceCode" '())))
+
+(define (python->texmacs x . opts)
+  (code->texmacs x))
+
+(define (python-snippet->texmacs x . opts)
+  (code-snippet->texmacs x))
+
+(converter texmacs-tree python-document
+  (:function texmacs->python))
+
+(converter python-document texmacs-tree
+  (:function python->texmacs))
+  
+(converter texmacs-tree python-snippet
+  (:function texmacs->python))
+
+(converter python-snippet texmacs-tree
+  (:function python-snippet->texmacs))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Plugin configuration
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Basically, the serializer makes the input preserve the newlines
+;; and adds the string character "\n<EOF>\n" by the end.
+;; I guess it could send "\x04" instead to signal a real EOF,
+;; but I would need to check if that does not kill the pipe...
+;; An alternative approach is to use the input-done? command
+;; from TeXmacs, but, at the time of this writing, it did not work.--A
+
+(define (python-serialize lan t)
+  (with u (pre-serialize lan t)
+    (with s (texmacs->code (stree->tree u) "SourceCode")
+      (string-append  s  "\n<EOF>\n"))))
+
+(define (python-utf8-command)
+  (string-append (url->system (find-binary-python3)) " -X utf8 "))
+
+(define (python-launcher)
+  (if (url-exists? "$TEXMACS_HOME_PATH/plugins/python")
+      (string-append (python-utf8-command)
+       (string-quote
+        (url->string
+         (string->url
+          "$TEXMACS_HOME_PATH/plugins/python/bin/python.pex"))))
+      (string-append (python-utf8-command)
+       (string-quote
+        (url->string
+         (string->url
+          "$TEXMACS_PATH/plugins/python/bin/python.pex"))))))
+
+(plugin-configure python
+  (:require (has-binary-python3?))
+  (:launch ,(python-launcher))
+  (:tab-completion #t)
+  (:serializer ,python-serialize)
+  (:session "Python")
+  (:scripts "Python"))
+
+;(set-session-multiline-input "python" "default" #t)
+;(set-program-multiline-input "python" "default" #t)
+
+(when (supports-python?)
+  (import-from (python-widgets) (python-menus)))
