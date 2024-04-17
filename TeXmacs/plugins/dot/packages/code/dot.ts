45cf16c4b2f26d751e94ea022216d113afeeb5e1
Darcy Shen <da@liii.pro>
2024-04-17 21:00:21 +0800

    wip

--- TeXmacs/plugins/dot/packages/code/dot.ts
+++ TeXmacs/plugins/dot/packages/code/dot.ts
@@ -20,6 +20,8 @@
     </src-license>
   </src-title>>
 
+  <use-module|(data dot)>
+
   <assign|dot-lang|<macro|body|<with|mode|prog|prog-language|dot|font-family|rm|<arg|body>>>>
 
   <assign|dot-code|<\macro|body>
