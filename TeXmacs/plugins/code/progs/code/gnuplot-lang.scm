38a2dee904b41119e57aca3faf694786b198e271
Darcy Shen <da@liii.pro>
2024-04-17 16:19:54 +0800

    wip

---
+++ TeXmacs/plugins/code/progs/code/gnuplot-lang.scm
@@ -0,0 +1,21 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : gnuplot-lang.scm
+;; DESCRIPTION : the Gnuplot Language
+;; COPYRIGHT   : (C) 2024  Darcy Shen
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(texmacs-module (code gnuplot-lang)
+  (:use (prog default-lang)))
+
+(tm-define (parser-feature lan key)
+  (:require (and (== lan "dot") (== key "keyword")))
+  `(,(string->symbol key)
+    (keyword
+      "set" "plot")))
