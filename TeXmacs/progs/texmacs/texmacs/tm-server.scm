5835455cc4881944faafb8495bd6ba6182c149b2
Darcy Shen <da@liii.pro>
2024-05-10 09:13:40 +0800

    [20_10] Fix Edit->Copy to->Image by setting texmacs->image:format to png
    ## What
    `Edit->Copy to->Image` does not work because `(get-preference
    "texmacs->image:format")` is `"default"`
    
    ## How to test your changes?
    + [x] Linux
    + [ ] macOS
    + [x] Windows
    
    Select something in the buffer, and click `Edit->Copy to->Image` and
    then paste it somewhere else.

--- TeXmacs/progs/texmacs/texmacs/tm-server.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-server.scm
@@ -132,7 +132,8 @@
   ("gui:line-input:autocommit" "on" noop)
   ("use native menubar" (get-default-native-menubar) noop)
   ("use unified toolbar" (get-default-unified-toolbar) noop)
-  ("tm format without utf8" "on" noop))
+  ("tm format without utf8" "on" noop)
+  ("texmacs->image:format" "png" noop))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Properties of some built-in routines
