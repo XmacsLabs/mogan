1cbf823443007d620962ce6e852577e68e447462
Darcy Shen <da@liii.pro>
2024-05-15 21:06:21 +0800

    [61_7] S7: update keywords for s7 scheme language

--- TeXmacs/plugins/s7/progs/code/s7-lang.scm
+++ TeXmacs/plugins/s7/progs/code/s7-lang.scm
@@ -22,17 +22,71 @@
     (declare_type
      "define" "set!" "lambda" "define-macro" "define-constant" "let")
     (keyword
-     "bignum" "string->number" "number->string" "string?" "length"
-     "append" "map" "for-each" "list" "cons" "cdr"  "procedure-source"
-     "sin" "cos" "sinh" "cosh" "tanh" "asinh" "acosh" "atanh" "expt" "sqrt"
-     "logior" "logxor" "logand" "lognot" "logbit?" "ash" "integer-decode-float"
-     "random" "nan?" "infinite?" "nan" "nan-payload"
-     "exact?" "rational?" "rationalize" "floor?" "remainder" "modulo" "lcm" "log"
-     "complex" "integer?")
+     "eq?" "bignum" "length" "append" "procedure-source"
+     ; SRFI-1: List constructors
+     "list" "cons" "xcons" "cons*" "make-list"
+     "list-tabulate" "list-copy" "circular-list" "iota"
+     ; SRFI-1: List predicates
+     "pair?" "null?" "proper-list?" "circular-list?" "dotted-list?"
+     "not-pair?" "null-list?" "list="
+     ; SRFI-1: List selectors
+     "car" "cdr" "caar" "cadr" "list-ref"
+     "first" "second" "third" "fourth" "fifth"
+     "take" "drop" "take-right" "drop-right" "last"
+     ; SRFI-1: MISC
+     "concatenate" "reverse" "append-reverse" "zip" "count"
+     ; SRFI-1: fold, unfold, map
+     "fold" "fold-right" "reduce" "reduce-right" "map"
+     "unfold" "unfold-right" "for-each" "map-in-order"
+     ; SRFI-1: Filtering & Parititioning
+     "filter" "parition" "remove"
+     ; SRFI-1: Searching
+     "find" "find-tail" "take-while" "drop-while" "span"
+     "any" "every" "list-index" "member" "memq" "memv"
+     ; SRFI-1: Deletion
+     "delete" "delete-duplicates"
+     ; SRFI-13: String predicates
+     "string?" "string-null?" "string-every" "string-any"
+     ; SRFI-13: String constructors
+     "make-string" "string" "string-tabulate"
+     ; SRFI-13: List & String Conversion
+     "string->list" "list->string" "reverse-list->string" "string-join"
+     ; SRFI-13: String selection
+     "string-length" "string-ref" "string-copy" "substring" "string-copy!"
+     "string-take" "string-take-right" "string-drop" "string-drop-right" "string-pad"
+     "string-pad-right" "string-trim" "string-trim-right" "string-trim-both"
+     ; SRFI-13: String comparison
+     "string-compare" "string=" "string<>"
+     ; SRFI-13: String Prefixes & Suffixes
+     "string-prefix?" "string-suffix?"
+     ; SRFI-13: String searching
+     "string-index" "string-index-right" "string-skip" "string-skip-right" "string-count"
+     "string-contains"
+     ; SRFI-13: String insertion and parsing
+     "string-replace" "string-tokenize"
+     ; SRFI-13: Filtering & Deleting
+     "string-filter" "string-delete"
+     ; SRFI-60: Integers as Bits
+     "logand" "logior" "logxor" "lognot" "logand"
+     "logbit?" "ash"
+     ; SRFI-70: Numbers
+     "number?" "complex?" "real?" "rational?" "integer?"
+     "exact?" "inexact?" "finite?" "infinite?" "zero?"
+     "positive?" "negative?" "odd?" "even?" "floor?"
+     "max" "min" "abs" "quotient" "remainder" "modulo"
+     "gcd" "lcm" "numerator" "denominator" "floor"
+     "ceiling" "truncate" "round" "rationalize"
+     "expt" "log" "complex" "real-part" "imag-part"
+     "sin" "cos" "tan" "asin" "acos" "atan"
+     "sinh" "cosh" "tanh" "asinh" "acosh" "atanh"
+     "sqrt" "expt" "make-rectangular" "make-polar" "magnitude"
+     "angle" "exact->inexact" "inexact->exact" "string->number" "number->string"
+     ; MISC
+     "integer-decode-float" "random" "nan?" "nan" "nan-payload")
     (keyword_conditional
-      "if" "cond" "else")
+     "if" "cond" "else")
     (keyword_control
-      "begin" "error" "catch" "throw")))
+     "begin" "error" "catch" "throw")))
 
 (tm-define (parser-feature lan key)
   (:require (and (== lan "s7") (== key "operator")))
