ca7f024927ef23d70c463953766eefa3ccac374f
Darcy Shen <da@liii.pro>
2024-06-06 23:38:08 +0800

    [75_27] bump to lolly 1.4.23

--- xmake/packages/l/lolly/xmake.lua
+++ xmake/packages/l/lolly/xmake.lua
@@ -24,7 +24,7 @@ package("lolly")
 
     add_urls("https://github.com/XmacsLabs/lolly.git")
     add_urls("https://gitee.com/XmacsLabs/lolly.git")
-    add_versions("1.4.22", "v1.4.22")
+    add_versions("1.4.23", "v1.4.23")
 
     add_deps("tbox")
     if not is_plat("wasm") then
