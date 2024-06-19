3084b7e0866aeb3e11c0953e248dd5c7389dc0d4
Darcy Shen <da@liii.pro>
2024-06-19 15:33:07 +0800

    wip

--- TeXmacs/progs/texmacs/texmacs/tm-server.scm
+++ TeXmacs/progs/texmacs/texmacs/tm-server.scm
@@ -131,7 +131,6 @@
   ("gui:line-input:autocommit" "on" noop)
   ("use native menubar" (get-default-native-menubar) noop)
   ("use unified toolbar" (get-default-unified-toolbar) noop)
-  ("tm format without utf8" "on" noop)
   ("texmacs->image:format" "png" noop))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
