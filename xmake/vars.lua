8be22b93a9245a4c988e0a586b7c2f45eb161dd3
Darcy Shen <da@liii.pro>
2024-06-19 10:29:05 +0800

    fix

--- xmake/vars.lua
+++ xmake/vars.lua
@@ -19,7 +19,7 @@ STABLE_VERSION = TEXMACS_VERSION
 STABLE_RELEASE = 1
 
 -- XmacsLabs dependencies
-LOLLY_VERSION = "1.4.24"
+LOLLY_VERSION = "1.4.26"
 
 -- Third-party dependencies
 S7_VERSION = "20230413"
