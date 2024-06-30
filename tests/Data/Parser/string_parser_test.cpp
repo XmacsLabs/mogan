085415fd719968eb1f9792c582a193a85f1c273b
UnbSky <74008999+UnbSky@users.noreply.github.com>
2024-06-30 20:18:06 +0800

    [14_12] Added test cases for src/Data/Parser/string_parser.cpp
    <!-- Thank you for your contribution! -->
    ## What
    Added test cases for src/Data/Parser/string_parser.cpp
    ## Why
    The current string_parser can handle most strings, but it cannot handle
    the **'** or **"** in the string
    ## How to test your changes?
    ```
    xmake build string_parser_test
    xmake r string_parser_test
    ```
    
    ---------
    
    Co-authored-by: Your Name <9838919+unbsky@user.noreply.gitee.com>

---
+++ tests/Data/Parser/string_parser_test.cpp
@@ -0,0 +1,60 @@
+
+/******************************************************************************
+ * MODULE     : string_parser_test.cpp
+ * DESCRIPTION: Properties of String Parser
+ * COPYRIGHT  : (C) 2024 UnbSky
+ *******************************************************************************
+ * This software falls under the GNU general public license version 3 or later.
+ * It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+ * in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+ ******************************************************************************/
+
+#include "analyze.hpp"
+#include "iterator.hpp"
+#include "string_parser.hpp"
+
+#include "converter.hpp"
+#include <QtTest/QtTest>
+
+class TestStringParser : public QObject {
+  Q_OBJECT
+
+private slots:
+  void test_can_parse ();
+  void test_parse ();
+};
+
+void
+TestStringParser::test_can_parse () {
+  string_parser_rep       string_parser= string_parser_rep ();
+  int                     pos;
+  hashmap<string, string> pairs;
+  pairs ("\"")= "\"";
+  pairs ("\'")= "\'";
+  string_parser.set_pairs (pairs);
+
+  pos= 4;
+  QVERIFY (string_parser.can_parse ("a = \"string\";", pos));
+}
+
+void
+TestStringParser::test_parse () {
+  string_parser_rep       string_parser= string_parser_rep ();
+  int                     pos;
+  hashmap<string, string> pairs;
+  pairs ("\"")= "\"";
+  pairs ("\'")= "\'";
+  string_parser.set_pairs (pairs);
+
+  pos= 4;
+  string_parser.parse ("a = \"string\";", pos);
+  QCOMPARE (pos, 12);
+
+  // The string parser cannot correctly handle quotation marks in strings
+  // pos = 4;
+  // string_parser.parse ("a = \"stri\\\"ng\";", pos);
+  // QCOMPARE (pos, 14);
+}
+
+QTEST_MAIN (TestStringParser)
+#include "string_parser_test.moc"
