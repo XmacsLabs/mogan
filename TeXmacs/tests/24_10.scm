1feedfeeb2bbe6e260d759f5ff3d00bdadd968f5
Darcy Shen <da@liii.pro>
2024-07-12 19:18:08 +0800

    wip

--- TeXmacs/tests/24_10.scm
+++ TeXmacs/tests/24_10.scm
@@ -1,3 +1,16 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : 24_10.scm
+;; DESCRIPTION : Test for task 24_10
+;; COPYRIGHT   : (C) 2024  Darcy Shen
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (define (export-as-html-and-load path)
   (with path (string-append "$TEXMACS_PATH/tests/tmu/" path)
     (with tmpfile (url-temp)
