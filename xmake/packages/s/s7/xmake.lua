0553fc288d30cfe592781e20bd1739f0eaa8238c
Darcy Shen <da@liii.pro>
2024-07-13 22:48:22 +0800

    20231016

--- xmake/packages/s/s7/xmake.lua
+++ xmake/packages/s/s7/xmake.lua
@@ -26,7 +26,7 @@ package("s7")
     add_urls("https://github.com/XmacsLabs/s7.git")
     add_urls("https://gitee.com/XmacsLabs/s7.git")
 
-    add_versions("20230913", "20230913")
+    add_versions("20231016", "20231016")
 
     add_configs("gmp", {description = "enable gmp support", default = false, type = "boolean"})
 
