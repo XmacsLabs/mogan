534db3800ca2406b56a78cb0523eaed271b4560e
Darcy Shen <da@liii.pro>
2024-07-14 23:45:29 +0800

    [61_8] Add scheme test on kernel/logic
    ## What
    + Add `define-library`, `import` in boot.scm
    + Enable `logic-test.scm`
    + Add tests under TeXmacs/progs/kernel to scheme tests
    + `(exit -1)` when there are failures
    
    ## Why
    First attempt of the check test frame.
    
    ## How to test your changes?
    CI

--- TeXmacs/progs/kernel/logic/logic-test.scm
+++ TeXmacs/progs/kernel/logic/logic-test.scm
@@ -16,23 +16,21 @@
 (texmacs-module (kernel logic logic-test)
   (:use (kernel logic logic-rules) (kernel logic logic-query)))
 
+(import (srfi srfi-78))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Examples of rules
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (logic-rules
-  ((sun% Joris Piet))
-  ((sun% Piet Opa))
+  ((son% Joris Piet))
+  ((son% Piet Opa))
   ((daughter% Geeske Opa))
   ((daughter% Jekke Opa))
-  ((child% 'x 'y) (sun% 'x 'y))
-  ((child% 'x 'y) (daughter% 'x 'y))
+  ((child% 'x 'y) (son% 'x 'y)) ; x is the son of y => x is the child of y
+  ((child% 'x 'y) (daughter% 'x 'y)) ; x is the daughter of y => x is the child of y
   ((descends% 'x 'y) (child% 'x 'y))
   ((descends% 'x 'z) (child% 'x 'y) (descends% 'y 'z)))
-
-;; (logic-query (child% 'x Opa))
-;; (logic-query (descends% 'x 'y))
-;; (logic-query (descends% 'x 'y) (daughter% Joleen Opa))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Addional assumptions <-> creating modules
@@ -40,14 +38,47 @@
 
 (logic-rules
   (assume family%) ; added to constraints for all rules below
-  ((sun% Joris Piet))
-  ((sun% Piet Opa))
+  ((son% Joris Piet))
+  ((son% Piet Opa))
   ((daughter% Geeske Opa))
   ((daughter% Jekke Opa))
-  ((child% 'x 'y) (sun% 'x 'y))
+  ((child% 'x 'y) (son% 'x 'y))
   ((child% 'x 'y) (daughter% 'x 'y))
   ((descends% 'x 'y) (child% 'x 'y))
   ((descends% 'x 'z) (child% 'x 'y) (descends% 'y 'z)))
 
 ;; (logic-query (child% 'x Opa))
+;; (logic-query (descends% 'x 'y))
+;; (logic-query (descends% 'x 'y) (daughter% Joleen Opa))
+;; (logic-query (child% 'x Opa))
 ;; (logic-query (child% 'x Opa) family%)
+
+(check
+  (logic-query (child% 'x Opa)) ; who is the child of Opa
+  =>
+  '(((x . Piet)) ((x . Geeske)) ((x . Jekke))))
+
+(define (test-doc) (display 'test-doc))
+(define (test-concat) (display 'test-concat))
+(define (test-href) (display 'test-href))
+
+(logic-dispatcher test-dispatcher%
+  (doc test-doc)
+  (concat test-concat)
+  (href test-href))
+
+(logic-rules
+  ((test-methods% 'x 'y) (test-dispatcher% 'x 'y)))
+
+(check (logic-ref test-methods% 'doc)
+  =>
+  test-doc)
+
+(check (logic-ref test-dispatcher% 'doc)
+  =>
+  test-doc)
+
+(tm-define (regtest-logic)
+  (check-report)
+  (if (check-failed?)
+    (exit -1)))
