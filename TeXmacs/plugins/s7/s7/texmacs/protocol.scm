af12d31745d52eaebaff99f33d104fb8087ccbec
Darcy Shen <da@liii.pro>
2024-07-17 14:40:04 +0800

    wip

---
+++ TeXmacs/plugins/s7/s7/texmacs/protocol.scm
@@ -0,0 +1,37 @@
+;
+; Copyright (C) 2024 The S7 SRFI Authors
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations
+; under the License.
+;
+
+(define-library (scheme base)
+(export (flush-verbatim))
+(begin
+(define (data-begin)
+  (display (integer->char 2)))
+
+(define (data-end)
+  (display (integer->char 5))
+  (flush-output-port))
+
+(define (data-escape)
+  (write (integer->char 27)))
+
+(define (flush-verbatim msg)
+  (data-begin)
+  (display "verbatim:")
+  (display msg)
+  (data-end))
+
+)
+)
--- TeXmacs/plugins/s7/s7/tm-s7.scm
+++ TeXmacs/plugins/s7/s7/tm-s7.scm
@@ -1,8 +1,50 @@
+;
+; Copyright (C) 2024 The S7 SRFI Authors
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations
+; under the License.
+;
+
 (set! *load-path*
-  (cons (append (get-env "TEXMACS_HOME_PATH") "plugins/s7/s7")
+  (cons (append (getenv "TEXMACS_HOME_PATH") "plugins/s7/s7")
         *load-path*))
 
+(import (texmacs protocol))
+
 (define (s7-welcome)
-  (display "S7 Scheme: ")
-  (display (substring (*s7* 'version) 3))
-  (newline))
+  (flush-verbatim
+    (append "S7 Scheme: " (substring (*s7* 'version) 3))))
+
+(define (eval-and-print line)
+  (let ((result (eval-string line)))
+    (if result
+      (begin 
+        (display "<=\n")
+        (display "=> ")
+        (display result)
+        (display "<=\n")))))
+
+(define (read-eval-print)
+  (let ((line (read-line *stdin*)))
+    (cond 
+      ((string=? line "\n")
+       #t)
+      (else
+       (eval-and-print line)
+       #t))))
+
+(define (loop)
+  (if (read-eval-print)
+      (loop)
+      (display "Bye!\n")))
+
+(s7-welcome)
