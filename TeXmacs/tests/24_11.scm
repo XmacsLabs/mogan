9ab4aeb3fa141704df65be64030d237ac41e531d
ATQlove <1284901271@qq.com>
2024-07-22 10:40:22 +0800

    [24_11] Tests for self-close tags

--- TeXmacs/tests/24_11.scm
+++ TeXmacs/tests/24_11.scm
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; MODULE      : test-htmlout.scm
-;; DESCRIPTION : Tests for htmlout.scm changes
+;; MODULE      : 24_11.scm
+;; DESCRIPTION : Tests for self-close tags
 ;; COPYRIGHT   : (C) 2024  ATQlove
 ;;
 ;; This software falls under the GNU general public license version 3 or later.
@@ -10,13 +10,17 @@
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;; Using (load ...) instead of (use-modules ...) allows overriding definitions 
+;; in the file. This lets us redefine functions like htmlout-text to capture 
+;; output for testing.
 (load "./TeXmacs/plugins/html/progs/convert/html/htmlout.scm")
 (import (srfi srfi-78))
 
 ;; Define a global variable to capture output
 (define *output* "")
 
-;; Define htmlout-text function to record output
+;; Override htmlout-text to capture output
+;; Necessary for testing by appending generated HTML to *output*
 (define (htmlout-text . args)
   (set! *output* (string-append *output* (apply string-append args))))
 
@@ -26,19 +30,6 @@
      (test))
    tests))
 
-(define (assert-equal actual expected)
-  (if (equal? actual expected)
-      (display "Test passed.\n")
-      (begin
-        (display "Test failed.\n")
-        (display "Expected: ")
-        (display expected)
-        (display "\n")
-        (display "Actual: ")
-        (display actual)
-        (display "\n"))))
-
-;; Ensure attribute names are symbols and attribute values are strings
 (define (format-attributes attrs)
   (map (lambda (attr) (list (car attr) (cdr attr))) attrs))
 
@@ -50,19 +41,31 @@
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'br '())
      (htmlout-close 'br)
-     (assert-equal *output* "<br />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<br />"))
    ;; hr
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'hr '())
      (htmlout-close 'hr)
-     (assert-equal *output* "<hr />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<hr />"))
    ;; wbr
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'wbr '())
      (htmlout-close 'wbr)
-     (assert-equal *output* "<wbr />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<wbr />"))
 
 
    ;; Group 2: Self-closing tags with attributes
@@ -71,61 +74,101 @@
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'area (format-attributes '((shape . "rect") (coords . "34,44,270,350") (alt . "Computer") (href . "computer.htm"))))
      (htmlout-close 'area)
-     (assert-equal *output* "<area />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<area />"))
    ;; base
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'base (format-attributes '((href . "https://example.com/"))))
      (htmlout-close 'base)
-     (assert-equal *output* "<base />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<base />"))
    ;; col
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'col (format-attributes '((span . "2") (class . "col-class"))))
      (htmlout-close 'col)
-     (assert-equal *output* "<col />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<col />"))
    ;; embed
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'embed (format-attributes '((src . "video.mp4") (type . "video/mp4"))))
      (htmlout-close 'embed)
-     (assert-equal *output* "<embed />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<embed />"))
    ;; img
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'img (format-attributes '((src . "image.png") (alt . "image"))))
      (htmlout-close 'img)
-     (assert-equal *output* "<img />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<img />"))
    ;; input
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'input (format-attributes '((type . "text") (value . "sample input"))))
      (htmlout-close 'input)
-     (assert-equal *output* "<input />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<input />"))
    ;; link
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'link (format-attributes '((rel . "stylesheet") (href . "styles.css"))))
      (htmlout-close 'link)
-     (assert-equal *output* "<link />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<link />"))
    ;; meta
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'meta (format-attributes '((charset . "UTF-8"))))
      (htmlout-close 'meta)
-     (assert-equal *output* "<meta />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<meta />"))
    ;; source
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'source (format-attributes '((src . "movie.mp4") (type . "video/mp4"))))
      (htmlout-close 'source)
-     (assert-equal *output* "<source />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<source />"))
    ;; track
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'track (format-attributes '((kind . "subtitles") (src . "subtitles_en.vtt") (srclang . "en") (label . "English"))))
      (htmlout-close 'track)
-     (assert-equal *output* "<track />\n"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<track />"))
      
 
    ;; Group 3: Non-self-closing tags example
@@ -134,18 +177,26 @@
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'a (format-attributes '((href . "https://example.com") (title . "Example"))))
      (htmlout-close 'a)
-     (assert-equal *output* "<a>\n\n</a>"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<a></a>"))
    ;; div
    (lambda ()
      (set! *output* "")  ; Reset output
      (htmlout-open-tags 'div (format-attributes '((class . "container") (id . "main"))))
      (htmlout-close 'div)
-     (assert-equal *output* "<div>\n\n</div>"))
+     (check
+      (begin
+        *output*)
+      =>
+      "<div></div>"))
    ))
 
-(tm-define (test_24_10)
-  ;; Run all tests and count the number of tests
-  (let ((n (length tests)))
-    (run-tests tests)
-    (display* "Total: " (object->string n) " tests.\n")
-    (display "Test suite of 24_10: ok\n")))
+(tm-define (test_24_11)
+  (run-tests tests)
+  (check-report)
+  (if (check-failed?)
+    (exit -1))
+    (display "Test suite of 24_11 end\n"))
