aea1cb9e352fae417d73aa3bfc1f14dba525118d
沈浪熊猫儿 <da@liii.pro>
2024-07-22 14:20:23 +0000

    [61_13] Chez: display the welcome message The first pull request of the Chez Scheme plugin. Just display the welcome message.
    Make Mogan the best software to learn scheme.
    
    Just `Insert -> Session -> Chez Scheme` on macOS or GNU/Linux.

---
+++ TeXmacs/plugins/chez/progs/init-chez.scm
@@ -0,0 +1,27 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : init-chez.scm
+;; DESCRIPTION : Initialize Chez Scheme plugin
+;; COPYRIGHT   : (C) 2024   Darcy Shen
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(use-modules (binary chez))
+
+(define (chez-launcher)
+  (string-append (url->system (find-binary-chez))
+    " --libdirs "
+    (string-append (url->system (get-texmacs-path)) "/plugins/chez/chez")
+    " --script "
+    (url->system (get-texmacs-path))
+    "/plugins/chez/chez/tm-chez.scm"))
+
+(plugin-configure chez
+  (:require (has-binary-chez?))
+  (:launch ,(chez-launcher))
+  (:session "Chez Scheme"))
