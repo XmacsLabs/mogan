5d8229d030ec49f74f3f69fdf51df9df22d8e008
ATQlove <kai.w27@foxmail.com>
2024-08-04 12:30:50 +0800

    Add docx converter & test

---
+++ TeXmacs/tests/24_15.scm
@@ -0,0 +1,40 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : 24_15.scm
+;; DESCRIPTION : Test for docx.scm
+;; COPYRIGHT   : (C) 2024  ATQlove
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(import (srfi srfi-78))
+(use-modules (data docx))
+
+(define (tm->docx tm-file-url docx-file-url)
+  ; Step 1: load the tm file
+  ; (display* "load-buffer: " tm-file-url "\n")
+  (load-buffer tm-file-url)
+  ; (display* "buffer-loaded: " tm-file-url "\n")
+
+  ; Step 2: Export the buffer to the docx file url
+  ; (display* "export buffer to: " docx-file-url "\n")
+  (export-to-docx tm-file-url docx-file-url)
+
+  ; Check if the docx file exists
+  (if (url-exists? docx-file-url)
+      (display* "Export successful: " docx-file-url "\n")
+      (display* "Export failed: " docx-file-url "\n"))
+
+  (url-exists? docx-file-url))
+
+(define (test_24_15)
+  (display  (url-exists? (system->url "$TEXMACS_PATH/tests/tm/24_14.tm")))
+  (let* ((tm-url "$TEXMACS_PATH/tests/tm/24_14.tm")
+         (docx-url "$TEXMACS_PATH/tests/tm/24_14.docx")
+         (result (tm->docx tm-url docx-url)))
+    (check result => #t)
+    (check-report)
+    (if (check-failed?) (exit -1))))
