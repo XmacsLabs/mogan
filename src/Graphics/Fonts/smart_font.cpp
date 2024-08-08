ff892eaa8f2c053313380d4d2aebf6803755a09b
Ke Shi <self.ke.shi@gmail.com>
2024-08-08 14:24:50 +0800

    [80_1] Fix math bracket fonts in Chinese docs
    <!-- Thank you for your contribution! -->
    ## What
    Fix math bracket fonts in Chinese docs.
    ## Why
    In Chinese documents, some math brackets display improperly.
    ## How to test your changes?
    Open `TeXmacs/tests/tm/80_1.tm` file in mogan.
    
    Before (wrong):
    <img
    src="https://github.com/user-attachments/assets/fb443910-3bae-4d0b-b30c-2935d2b24b49"
    width="500" alt="image">
    After (right):
    <img
    src="https://github.com/user-attachments/assets/b4ed411f-4426-4d3e-9acb-70e2408a3321"
    width="500" alt="image">

--- src/Graphics/Fonts/smart_font.cpp
+++ src/Graphics/Fonts/smart_font.cpp
@@ -1612,15 +1612,15 @@ smart_font_bis (string family, string variant, string series, string shape,
   if (starts (family, "sys-")) {
     if (family == "sys-chinese") {
       string name= default_chinese_font_name ();
-      family     = "cjk=" * name * ",roman";
+      family     = "math=roman,cjk=" * name * ",roman";
     }
     if (family == "sys-japanese") {
       string name= default_japanese_font_name ();
-      family     = "cjk=" * name * ",roman";
+      family     = "math=roman,cjk=" * name * ",roman";
     }
     if (family == "sys-korean") {
       string name= default_korean_font_name ();
-      family     = "cjk=" * name * ",roman";
+      family     = "math=roman,cjk=" * name * ",roman";
     }
   }
   family= tex_gyre_fix (family, series, shape);
