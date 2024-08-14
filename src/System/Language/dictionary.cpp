ce9463ea2a70438441812d32d454680a3803dd6a
Darcy Shen <da@liii.pro>
2024-08-14 10:58:36 +0800

    wip

--- src/System/Language/dictionary.cpp
+++ src/System/Language/dictionary.cpp
@@ -60,11 +60,7 @@ dictionary_rep::load (url u) {
       if (is_quoted (l)) l= scm_unquote (l);
       string r= t[i][1]->label;
       if (is_quoted (r)) r= scm_unquote (r);
-      if (to == "chinese" || to == "japanese" || to == "korean" ||
-          to == "taiwanese" || to == "russian" || to == "ukrainian" ||
-          to == "bulgarian" || to == "german" || to == "greek" ||
-          to == "slovak")
-        r= utf8_to_cork (r);
+      r        = utf8_to_cork (r);
       table (l)= r;
     }
 }
