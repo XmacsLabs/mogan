f63e5b3ed4ccc4d8d88f7e8c2a4d9e5168a3b8bd
Darcy Shen <da@liii.pro>
2024-08-29 18:16:53 +0800

    [43_10] Move latex-kbd to the latex plugin

---
+++ TeXmacs/plugins/latex/progs/init-latex.scm
@@ -0,0 +1,15 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : init-latex.scm
+;; DESCRIPTION : Initialize the LaTeX plugin
+;; COPYRIGHT   : (C) 2024   Darcy Shen
+;;
+;; This software falls under the GNU general public license and comes WITHOUT
+;; ANY WARRANTY WHATSOEVER. See the file $TEXMACS_PATH/LICENSE for details.
+;; If you don't have this file, write to the Free Software Foundation, Inc.,
+;; 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(import-from (latex-kbd))
