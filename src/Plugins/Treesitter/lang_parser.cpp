a6df864ac9fc19087a0760af1acb1d9d070f20a2
UnbSky <531149301@qq.com>
2024-09-21 21:03:25 +0800

    minor change

--- src/Plugins/Treesitter/lang_parser.cpp
+++ src/Plugins/Treesitter/lang_parser.cpp
@@ -94,11 +94,11 @@ lang_parser::get_root_node (tree t, int& start_index, int& hash_code) {
     // cout << "Father: " << father_ip << " Root:" << root << LF;
     if (!is_atomic (root)) {
       // Fix some root node cannot find
-      tree env_child= the_drd->get_env_child (root, tree_index, MODE, "");
+      tree env_child_mode= the_drd->get_env_child (root, tree_index, MODE, "");
 
       tree env_child_pl=
-          the_drd->get_env_child (root, tree_index, "prog-language", "");
-      if (env_child == "prog" || N (env_child_pl) > 0) {
+          the_drd->get_env_child (root, tree_index, moebius::PROG_LANGUAGE, "");
+      if (env_child_mode == "prog" || N (env_child_pl) > 0) {
         root= root[tree_index];
         // cout << "Tree Index: " << tree_index << " Root: " << root << LF;
         break;
@@ -107,11 +107,9 @@ lang_parser::get_root_node (tree t, int& start_index, int& hash_code) {
   }
   if (N (father_ip) == 0) {
     // cout << "Can not meet Prog " << t << " IP:" << obtain_ip(t) << LF;
-    root= root[tree_index]; // Set Root To Self as Fallback
+    root= root[tree_index];
   }
   hash_code= hash (root);
-  // cout << "root " << root << " \nhash " << hash_code << LF;
-
   get_data_from_root (root, t, start_index);
 
   // //Add this to avoid wrong hash code
@@ -371,8 +369,6 @@ lang_parser::do_ast_parse (tree code_root) {
   // Tree Parse
   // time_t t1  = texmacs_time (); // Parse Time Start
   code_string= get_code_str (code_root);
-  // cout << "code_string \n=========================\n" << code_string <<
-  // "=========================\n";
   c_string source_code (code_string);
   TSTree*  tstree=
       ts_parser_parse_string (ast_parser, NULL, source_code, N (code_string));
