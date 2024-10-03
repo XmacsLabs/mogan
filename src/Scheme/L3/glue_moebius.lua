127f7cb8f9e4538997f67467a39605e141e91127
Darcy Shen <da@liii.pro>
2024-10-03 14:11:22 +0800

    [66_7] herk->utf8 and utf8->herk

--- src/Scheme/L3/glue_moebius.lua
+++ src/Scheme/L3/glue_moebius.lua
@@ -55,6 +55,22 @@ function main()
                     "string"
                 }
             },
+            {
+                scm_name = "utf8->herk",
+                cpp_name = "utf8_to_herk",
+                ret_type = "string",
+                arg_list = {
+                    "string"
+                }
+            },
+            {
+                scm_name = "herk->utf8",
+                cpp_name = "herk_to_utf8",
+                ret_type = "string",
+                arg_list = {
+                    "string"
+                }
+            },
               -- routines for strings in the TeXmacs encoding
             {
                 scm_name = "string->tmstring",
