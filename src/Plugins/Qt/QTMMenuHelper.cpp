86e39f1246b92dd44cf5150cb86dbff345829f15
Jim Zhou <jimzhouzzy@gmail.com>
2025-06-23 13:03:58 +0000

    !270 Add M-a/C-a event for QTMLineEdit

--- src/Plugins/Qt/QTMMenuHelper.cpp
+++ src/Plugins/Qt/QTMMenuHelper.cpp
@@ -530,9 +530,10 @@ QTMLineEdit::keyPressEvent (QKeyEvent* ev) {
         (ev->modifiers () & Qt::MetaModifier) == 0) {
       QLineEdit::keyPressEvent (ev);
     }
-    else if ((last_key == 'C' || last_key == 'V' || last_key == 'X') &&
+    else if ((last_key == 'C' || last_key == 'V' || last_key == 'X' ||
+              last_key == 'A') &&
              (ev->modifiers () == Qt::ControlModifier)) {
-      // M-x/M-c/M-v on macOS or C-x/C-c/C-v on Linux and Windows
+      // M-x/M-c/M-v/M-a on macOS or C-x/C-c/C-v/C-a on Linux and Windows
       QLineEdit::keyPressEvent (ev);
     }
 
