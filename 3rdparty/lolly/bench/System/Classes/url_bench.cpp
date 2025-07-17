9649ece3ac19363f6ec985543b33a6462ea10e94
yifant <15799676+yifant1028@user.noreply.gitee.com>
2025-07-17 02:12:36 +0000

    !414 [207_5] 增加长URL的测试项

--- 3rdparty/lolly/bench/System/Classes/url_bench.cpp
+++ 3rdparty/lolly/bench/System/Classes/url_bench.cpp
@@ -11,6 +11,17 @@
 
 static ankerl::nanobench::Bench bench;
 
+url
+make_deep_url (int depth) {
+  std::string s;
+  s.reserve (depth * 2);
+  for (int i= 0; i <= depth; ++i) {
+    if (i) s.push_back ('/');
+    s.push_back ('x');
+  }
+  return url (s.c_str ());
+}
+
 int
 main () {
   lolly::init_tbox ();
@@ -22,4 +33,17 @@ main () {
   bench.run ("url descends concat 8", [&] {
     descends (url ("a/b/c/d/e/f/g/h/i/j"), url ("a/b/c/d/e/f/g/h"));
   });
+
+  bench.run ("as_string deep url (depth 1000)", [&] {
+    url    u= make_deep_url (1000);
+    string s= as_string (u, URL_STANDARD);
+    ankerl::nanobench::doNotOptimizeAway (s);
+  });
+  bench.run ("as_string deep url (depth 200)", [&] {
+    url    u= make_deep_url (200);
+    string s= as_string (u, URL_STANDARD);
+    ankerl::nanobench::doNotOptimizeAway (s);
+  });
+
+  return 0;
 }
