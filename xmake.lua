6cde0bdb11e7cdca409fe52544af4b0e98183bb9
沈达 <da@liii.pro>
2025-08-01 06:43:18 +0000

    !478 bump to Liii STEM v2025.1.0

--- xmake.lua
+++ xmake.lua
@@ -9,8 +9,8 @@
 -- It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
 -- in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
 
-set_project("Liii STEM Suite")
-local XMACS_VERSION="2025.0.11"
+set_project("Liii STEM")
+local XMACS_VERSION="2025.1.0"
 
 option("mupdf")
     set_default(false)
