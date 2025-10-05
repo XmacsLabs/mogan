eb15fd30f9e363d098d38f06389cfb0653be7452
Da Shen <da@liii.pro>
2025-10-05 13:46:49 +0800

    [200_22] bump to v2025.1.2

--- packages/debian/package.sh
+++ packages/debian/package.sh
@@ -8,7 +8,7 @@ fi
 
 APP_HOME="$(cd "${FWDIR}/../.."; pwd)"
 
-VERSION="2025.1.1"
+VERSION="2025.1.2"
 
 ln -s $APP_HOME/packages/debian $APP_HOME/debian
 cp $APP_HOME/debian/control.in $APP_HOME/debian/control
