143969f209ec469602a7d8ad6515a6f9eca4a2d0
Yuki <yifan@liii.pro>
2025-11-20 03:34:10 +0000

    !1007 [61_2] 添加homebrew下载的conda链接

--- TeXmacs/plugins/binary/progs/binary/conda.scm
+++ TeXmacs/plugins/binary/progs/binary/conda.scm
@@ -16,8 +16,10 @@
 
 (define (conda-binary-candidates)
   (cond ((os-macos?)
-         (list "/opt/anaconda3/bin/conda"
-               "$HOME/miniconda3/bin/conda"))
+         (list "/opt/homebrew/anaconda3/bin/conda"
+               "/opt/anaconda3/bin/conda"
+               "/opt/homebrew/miniconda3/bin/conda"
+               "/opt/miniconda3/bin/conda"))
         ((os-win32?)
          (list "$USERPROFILE/anaconda3/Scripts/conda.exe"
                "$PROGRAMDATA/anaconda3/Scripts/conda.exe"
