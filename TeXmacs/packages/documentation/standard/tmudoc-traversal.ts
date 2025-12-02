9951c66bb4e6eec1cfda589f74166823d311dba3
Saidealem Eksan <eksan@liii.pro>
2025-12-02 14:40:00 +0000

    !1115 [204_10] 新增mogandoc样式-标题的logo换为墨干的logo

---
+++ TeXmacs/packages/documentation/standard/tmudoc-traversal.ts
@@ -0,0 +1,176 @@
+<TeXmacs|2.1.4>
+
+<style|source>
+
+<\body>
+  <active*|<\src-title>
+    <src-package|mogandoc-traversal|1.0>
+
+    <\src-purpose>
+      This package contains macros which allow for the automatic generation
+      or extraction of documentation from small files. For instance, there
+      are tags which indicate how to traverse the documentation.
+    </src-purpose>
+
+    <src-copyright|2001--2004|Joris van der Hoeven>
+
+    <\src-license>
+      This software falls under the <hlink|GNU general public license,
+      version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+      WARRANTY WHATSOEVER. You should have received a copy of the license
+      which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+    </src-license>
+  </src-title>>
+
+  <\active*>
+    <\src-comment>
+      Decorations for title-bars with gnus.
+    </src-comment>
+  </active*>
+
+  <assign|tmdoc-lgnu|<macro|<active*|<image|local:$TEXMACS_PATH/misc/images/tm_gnu1.ps|0.25w|0.25h||>>>>
+
+  <assign|tmdoc-rgnu|<macro|<active*|<image|local:$TEXMACS_PATH/misc/images/tm_gnu2.ps|0.25w|0.25h||>>>>
+
+  <assign|tmdoc-gnu-title|<macro|title|<tabular*|<tformat|<twith|table-width|0.99par>|<cwith|1|1|2|2|cell-hpart|1>|<cwith|1|1|1|-1|cell-valign|c>|<cwith|1|1|1|1|cell-vcorrect|n>|<cwith|1|1|3|3|cell-vcorrect|n>|<cwith|1|1|2|2|cell-hyphen|c>|<cwith|1|1|1|1|cell-lsep|0fn>|<cwith|1|1|3|3|cell-rsep|0fn>|<table|<row|<cell|<tmdoc-lgnu>>|<\cell>
+    <\with|par-mode|center>
+      <arg|title>
+    </with>
+  </cell>|<cell|<tmdoc-rgnu>>>>>>>>
+
+  <assign|tmdoc-bar|<macro|content|<with|color|dark
+  grey|<block|<tformat|<cwith|1|1|1|1|cell-background|broken
+  white>|<twith|table-width|1par>|<cwith|1|1|1|1|cell-hyphen|t>|<cwith|1|1|1|1|cell-lborder|0.5ln>|<cwith|1|1|1|1|cell-rborder|0.5ln>|<cwith|1|1|1|1|cell-bborder|0.5ln>|<cwith|1|1|1|1|cell-tborder|0.5ln>|<table|<row|<cell|<with|color|black|<arg|content>>>>>>>>>>
+
+  <\active*>
+    <\src-comment>
+      Title bars.
+    </src-comment>
+  </active*>
+
+  <assign|tmdoc-underline|<\macro|body>
+    <tabular|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|1|cell-tborder|0ln>|<cwith|1|1|1|1|cell-bborder|1ln>|<cwith|1|1|1|1|cell-lborder|0ln>|<cwith|1|1|1|1|cell-rborder|0ln>|<cwith|1|1|1|1|cell-lsep|0spc>|<cwith|1|1|1|1|cell-rsep|0spc>|<cwith|1|1|1|1|cell-hyphen|t>|<cwith|1|1|1|1|cell-bsep|2spc>|<cwith|1|1|1|1|cell-tsep|1spc>|<table|<row|<\cell>
+      <arg|body>
+    </cell>>>>>
+  </macro>>
+
+  <assign|mogandoc-moganimage|<macro|<image|local:$TEXMACS_PATH/misc/images/stem-512.png|3em|3em||-0.3h>>>
+
+  <assign|tmdoc-title-font|<macro|title|<with|font-series|bold|font-shape|small-caps|font-size|1.6|<arg|title>>>>
+
+  <assign|mogandoc-title|<macro|title|<tmdoc-underline|<mogandoc-moganimage><space|1em><tmdoc-title-font|<arg|title>>><vspace|2fn>>>
+
+  <assign|mogandoc-title*|<\macro|title|b>
+    <tmdoc-underline|<mogandoc-moganimage><space|1em><tmdoc-title-font|<arg|title>>>
+
+    <tmdoc-underline|<arg|b>>
+
+    <vspace|2fn>
+  </macro>>
+
+  <assign|mogandoc-title**|<\macro|t|title|b>
+    <tmdoc-underline|<arg|t>>
+
+    <tmdoc-underline|<mogandoc-moganimage><space|1em><tmdoc-title-font|<arg|title>>>
+
+    <tmdoc-underline|<arg|b>>
+
+    <vspace|2fn>
+  </macro>>
+
+  <assign|tmdoc-title|<macro|title|<tmdoc-underline|<mogandoc-moganimage><space|1em><tmdoc-title-font|<arg|title>>><vspace|2fn>>>
+
+  <assign|tmdoc-title*|<\macro|title|b>
+    <tmdoc-underline|<mogandoc-moganimage><space|1em><tmdoc-title-font|<arg|title>>>
+
+    <tmdoc-underline|<arg|b>>
+
+    <vspace|2fn>
+  </macro>>
+
+  <assign|tmdoc-title**|<\macro|t|title|b>
+    <tmdoc-underline|<arg|t>>
+
+    <tmdoc-underline|<mogandoc-moganimage><space|1em><tmdoc-title-font|<arg|title>>>
+
+    <tmdoc-underline|<arg|b>>
+
+    <vspace|2fn>
+  </macro>>
+
+  <\active*>
+    <\src-comment>
+      Tags for displaying hyperlinks to all available translations of
+      documentation.
+
+      The flags were found in /usr/share/pixmaps/gkb.
+    </src-comment>
+  </active*>
+
+  <assign|tmdoc-flag|<macro|flag| <with|color|black|<block|<tformat|<cwith|1|1|1|1|cell-lsep|0fn>|<cwith|1|1|1|1|cell-rsep|0fn>|<cwith|1|1|1|1|cell-bsep|0fn>|<cwith|1|1|1|1|cell-tsep|0fn>|<cwith|1|1|1|1|cell-vcorrect|n>|<table|<row|<cell|<arg|flag>>>>>>>
+  >>
+
+  <assign|tmdoc-translation|<macro|name|suffix|<if|<unequal|<find-file|<merge|<arg|name>|<merge|<arg|suffix>|.tm>>>|false>|<hlink|<compound|tmdoc-flag|<image|<merge|https://www.texmacs.org/Images/flag|<merge|<arg|suffix>|.png>>|0.5w|0.5h||>>|<merge|<arg|name>|<merge|<arg|suffix>|.tm>>>>>>
+
+  <assign|tmdoc-translations|<\macro|name>
+    <\surround||<vspace|0.5fn>>
+      <\with|par-mode|center>
+        <tmdoc-translation|<arg|name>|.de><tmdoc-translation|<arg|name>|.en><tmdoc-translation|<arg|name>|.es><tmdoc-translation|<arg|name>|.fr><tmdoc-translation|<arg|name>|.it><tmdoc-translation|<arg|name>|.pt>
+      </with>
+    </surround>
+  </macro>>
+
+  <\active*>
+    <\src-comment>
+      The footers and copyright information.
+    </src-comment>
+  </active*>
+
+  <assign|tmdoc-overline|<\macro|body>
+    <tabular|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|1|cell-tborder|1ln>|<cwith|1|1|1|1|cell-bborder|0ln>|<cwith|1|1|1|1|cell-lborder|0ln>|<cwith|1|1|1|1|cell-rborder|0ln>|<cwith|1|1|1|1|cell-lsep|0spc>|<cwith|1|1|1|1|cell-rsep|0spc>|<cwith|1|1|1|1|cell-hyphen|t>|<cwith|1|1|1|1|cell-bsep|1spc>|<cwith|1|1|1|1|cell-tsep|2spc>|<table|<row|<\cell>
+      <arg|body>
+    </cell>>>>>
+  </macro>>
+
+  <assign|tmdoc-copyright-extra|<macro|name|, <arg|name>>>
+
+  <assign|tmdoc-copyright|<xmacro|x|<no-indent><vspace*|1fn><tmdoc-overline|<copyright><with|font-size|0.84|
+  \ <arg|x|0> <localize|by> <arg|x|1><map-args|tmdoc-copyright-extra|concat|x|2>>>>>
+
+  <assign|tmdoc-license|<macro|body|<with|color|grey|font-size|0.59|<with|language|english|<arg|body>>>>>
+
+  <\active*>
+    <\src-comment>
+      Tags for the traversal of documentation.
+    </src-comment>
+  </active*>
+
+  <assign|traverse|<\macro|body>
+    <\itemize>
+      <arg|body>
+    </itemize>
+  </macro>>
+
+  <assign|branch|<\macro|body|destination>
+    <item><hlink|<arg|body>|<tmdoc-file|<arg|destination>>>
+  </macro>>
+
+  <assign|extra-branch|<\macro|body|destination>
+    <item><hlink|<with|color|#806040|<arg|body>>|<tmdoc-file|<arg|destination>>>
+  </macro>>
+
+  <assign|continue|<\macro|body|destination>
+    <item><hlink|<with|color|#806040|<arg|body>>|<tmdoc-file|<arg|destination>>>
+  </macro>>
+
+  <assign|optional-branch|<\macro|body|destination>
+    <item><hlink|<with|color|#208080|<arg|body>>|<tmdoc-file|<arg|destination>>>
+  </macro>>
+
+  <assign|tmdoc-include|<\macro|inclusion>
+    <extern|tmdoc-include|<find-file|.|$TEXMACS_DOC_PATH|http://www.gnu.org/software/texmacs-doc|<quote-arg|inclusion>>>
+  </macro>>
+</body>
+
+<initial|<\collection>
+</collection>>
---
+++ TeXmacs/styles/documentation/Mogan/tmudoc.ts
@@ -0,0 +1,143 @@
+<TeXmacs|2.1.4>
+
+<style|source>
+
+<\body>
+  <\active*>
+    <\src-title>
+      <src-style-file|tmudoc|1.0>
+
+      <\src-purpose>
+        Style for the Mogan STEM documentation.
+      </src-purpose>
+
+      <\src-copyright|2002--2004>
+        Joris van der Hoeven
+      </src-copyright>
+
+      <\src-license>
+        This software falls under the <hlink|GNU general public license,
+        version 3 or later|$TEXMACS_PATH/LICENSE>. It comes WITHOUT ANY
+        WARRANTY WHATSOEVER. You should have received a copy of the license
+        which the software. If not, see <hlink|http://www.gnu.org/licenses/gpl-3.0.html|http://www.gnu.org/licenses/gpl-3.0.html>.
+      </src-license>
+    </src-title>
+  </active*>
+
+  <use-package|std|env|title-generic|header-article|section-article|doc-tmu>
+
+  <\active*>
+    <\src-comment>
+      Global style parameters.
+    </src-comment>
+  </active*>
+
+  <assign|par-hyphen|professional>
+
+  <assign|par-par-sep|0.5fn>
+
+  <assign|par-first|0fn>
+
+  <assign|padded-par-par-sep|0.5fn>
+
+  <assign|indent-par-first|1.5fn>
+
+  <assign|save-aux|false>
+
+  <assign|sectional-sep|<macro|.<space|2spc>>>
+
+  <assign|sectional-post-sep|<macro|<space|2spc>>>
+
+  <assign|html-css|tmdoc-custom.css>
+
+  <assign|html-head-javascript-src|https://www.texmacs.org/javascript/texmacs_functions.js>
+
+  <\active*>
+    <\src-comment>
+      Sections, subsections and subsubsections.
+    </src-comment>
+  </active*>
+
+  <assign|sectional-sep|<macro|.<space|2spc>>>
+
+  <assign|section-font|<macro|name|<large|<arg|name>>>>
+
+  <assign|subsection-font|<macro|name|<arg|name>>>
+
+  <assign|subsubsection-font|<macro|name|<arg|name>>>
+
+  \;
+
+  <assign|section-title|<macro|name|<style-with|src-compact|none|<sectional-normal-bold|<vspace*|1.25fn><section-font|<arg|name>><vspace|0.5fn>>>>>
+
+  <assign|subsection-title|<macro|name|<style-with|src-compact|none|<sectional-normal-bold|<vspace*|1fn><subsection-font|<arg|name>><vspace|0.5fn>>>>>
+
+  <assign|subsubsection-title|<macro|name|<style-with|src-compact|none|<sectional-normal-bold|<vspace*|0.5fn><subsubsection-font|<arg|name>><vspace|0.25fn>>>>>
+
+  <\active*>
+    <\src-comment>
+      Paragraphs and subparagraphs.
+    </src-comment>
+  </active*>
+
+  \;
+
+  <assign|paragraph-font|<macro|name|<with|font-shape|italic|<arg|name>>>>
+
+  <assign|subparagraph-font|<macro|name|<with|font-shape|italic|<arg|name>>>>
+
+  <assign|paragraph-title|<macro|name|<style-with|src-compact|none|<sectional-short|<vspace*|0.5fn><paragraph-font|<arg|name>>>>>>
+
+  <assign|subparagraph-title|<macro|name|<style-with|src-compact|none|<sectional-short|<vspace*|0.5fn><subparagraph-font|<arg|name>>>>>>
+
+  <\active*>
+    <\src-comment>
+      Other customizations.
+    </src-comment>
+  </active*>
+
+  <assign|enunciation-name|<macro|name|<with|font-shape|small-caps|<arg|name>>>>
+
+  <active*|<\src-comment>
+    Typewriter font.
+  </src-comment>>
+
+  <assign|tt-font-base|<macro|x|<with|font|roman|font-family|tt|magnification|<times|<value|magnification>|1.06>|<arg|x>>>>
+
+  <assign|tt-font|<macro|x|<tt-font-base|<arg|x>>>>
+
+  <assign|tt-prog-font|<macro|x|<tt-font-base|<arg|x>>>>
+
+  <assign|tt-session-font|<macro|x|<with|font|typewriter=CMU,roman|<arg|x>>>>
+
+  \;
+
+  <assign|verbatim|<macro|body|<with|language|verbatim|<tt-font|<arg|body>>>>>
+
+  <assign|shell|<macro|body|<with|mode|prog|prog-language|shell|<tt-prog-font|<arg|body>>>>>
+
+  <assign|scm|<macro|body|<with|mode|prog|prog-language|scheme|<tt-prog-font|<arg|body>>>>>
+
+  <assign|cpp|<macro|body|<with|mode|prog|prog-language|cpp|<tt-prog-font|<arg|body>>>>>
+
+  <assign|python|<macro|body|<with|mode|prog|prog-language|python|<tt-prog-font|<arg|body>>>>>
+
+  <active*|<\src-comment>
+    Hacks for the typewriter font in plug-in sessions.
+  </src-comment>>
+
+  <assign|scheme-input|<\macro|prompt|body>
+    <\with|generic-prompt-color|<value|scheme-prompt-color>|generic-input-color|<value|scheme-input-color>>
+      <tt-font|<generic-input|<arg|prompt>|<arg|body>>>
+    </with>
+  </macro>>
+
+  <assign|input|<macro|prompt|body|<with|mode|prog|font-family|rm|<tt-session-font|<style-with|src-compact|none|<compound|<style-with|src-compact|none|<if|<provides|<merge|<value|prog-language>|-input>>|<merge|<value|prog-language>|-input>|generic-input>>|<arg|prompt>|<arg|body>>>>>>>
+
+  <assign|output|<macro|body|<with|mode|prog|font-family|rm|<tt-session-font|<style-with|src-compact|none|<compound|<style-with|src-compact|none|<if|<provides|<merge|<value|prog-language>|-output>>|<merge|<value|prog-language>|-output>|generic-output>>|<arg|body>>>>>>>
+
+  <assign|script-input|<macro|language|session|in|out|<tt-session-font|<style-with|src-compact|none|<compound|<style-with|src-compact|none|<if|<provides|<merge|<arg|language>|-script-input>>|<merge|<arg|language>|-script-input>|generic-script-input>>|<arg|language>|<arg|session>|<arg|in>|<arg|out>>>>>>
+</body>
+
+<initial|<\collection>
+</collection>>
