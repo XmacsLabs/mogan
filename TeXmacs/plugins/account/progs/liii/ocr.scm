3cbf040cc141e6b52daa2a7c9c497f5d4bef3f67
MoonLL <zc15315336096@163.com>
2025-12-23 20:52:08 +0800

    [201_41] 版权信息

--- TeXmacs/plugins/account/progs/liii/ocr.scm
+++ TeXmacs/plugins/account/progs/liii/ocr.scm
@@ -1,3 +1,16 @@
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : ocr.scm
+;; DESCRIPTION : ocr
+;; COPYRIGHT   : (C) 2025  Mogan STEM authors
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 (texmacs-module (liii ocr))
 (import (liii os))
 (import (liii base64))
