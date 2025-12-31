c16cd7140feea69862fedc2e5937d8df3f728ea6
wumo <121778696+17802723@users.noreply.github.com>
2025-12-31 10:09:26 +0800

    [201_47 ] 退出搜索替换窗口时光标传入主文档 (#2411)

--- TeXmacs/progs/generic/search-widgets.scm
+++ TeXmacs/progs/generic/search-widgets.scm
@@ -140,7 +140,7 @@ search-buffer
 ----
 url
 如果当前缓冲区已经是 tmfs://aux/search 类型的辅助缓冲区，则返回该缓冲区URL；
-否则返回基于当前视图URL MD5哈希的唯一搜索缓冲区URL。
+否则返回基于当前视图URL MD5哈希的唯一搜索缓冲区URL和当前窗口。
 
 逻辑
 ----
@@ -156,10 +156,13 @@ url
 (tm-define (search-buffer)
   (with u (current-buffer)
     (if (and (url-rooted-tmfs? u)
-             (== (url-head u) (string->url "tmfs://aux/search")))
+             (== (url-head(url-head u)) (string->url "tmfs://aux/search")))
         u
         (string->url
-          (string-append "tmfs://aux/search/" (md5 (url->string (current-view-url))))))))
+          (string-append "tmfs://aux/search/" 
+                         (md5 (url->string (current-view-url)))
+                         "/"
+                         (url->string (url-tail (current-window))))))))
 
 #|
 replace-buffer
@@ -177,7 +180,7 @@ replace-buffer
 ----
 url
 如果当前缓冲区已经是 tmfs://aux/replace 类型的辅助缓冲区，则返回该缓冲区URL；
-否则返回基于当前视图URL MD5哈希的唯一替换缓冲区URL。
+否则返回基于当前视图URL MD5哈希的唯一替换缓冲区URL和当前窗口。
 
 逻辑
 ----
@@ -193,10 +196,16 @@ url
 (tm-define (replace-buffer)
   (with u (current-buffer)
     (if (and (url-rooted-tmfs? u)
-             (== (url-head u) (string->url "tmfs://aux/replace")))
+             (== (url-head(url-head u)) (string->url "tmfs://aux/replace")))
         u
         (string->url
-          (string-append "tmfs://aux/replace/" (md5 (url->string (current-view-url))))))))
+          (string-append "tmfs://aux/replace/" 
+                         (md5 (url->string (current-view-url)))
+                         "/"
+                         (url->string (url-tail (current-window))))))))
+
+(tm-define (auxiliary-buffer->window x)
+  (url-append (string->url "tmfs://window") (url-tail x)))
 
 (tm-define (master-buffer)
   (and (buffer-exists? (search-buffer))
@@ -427,7 +436,10 @@ url
   (set! search-serial (+ search-serial 1))
   (with-buffer (master-buffer)
     (cancel-alt-selection "alternate"))
-  (set-search-window-state #f #f))
+  (set-search-window-state #f #f)
+  (buffer-focus
+    (window->buffer
+      (auxiliary-buffer->window (search-buffer)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Replace occurrences
