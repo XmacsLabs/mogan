19a6e6763086167c126884b7d4840dfaf93bbdb8
wumo <he17802723@163.com>
2025-12-31 10:19:04 +0800

    improve

--- TeXmacs/progs/generic/search-kbd.scm
+++ TeXmacs/progs/generic/search-kbd.scm
@@ -1,3 +1,14 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; MODULE      : search-kbd.scm
+;; DESCRIPTION : search-widget kbd
+;; COPYRIGHT   : (C) 2025
+;;
+;; This software falls under the GNU general public license version 3 or later.
+;; It comes WITHOUT ANY WARRANTY WHATSOEVER. For details, see the file LICENSE
+;; in the root directory or <http://www.gnu.org/licenses/gpl-3.0.html>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (texmacs-module (generic search-kbd)
   (:use (generic search-widgets)))
