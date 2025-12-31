14a1cb32beef5847d7a38a327af2653e82ddd462
Yuki <yifan@liii.pro>
2025-12-31 09:57:18 +0800

    [202_101] 搜索/替换颜色和高亮颜色 (#2419)

--- TeXmacs/progs/generic/search-widgets.scm
+++ TeXmacs/progs/generic/search-widgets.scm
@@ -350,7 +350,7 @@ url
     (with-buffer (search-buffer)
       (if ok?
           (init-default "bg-color")
-          (init-env "bg-color" "#fff0f0")))
+          (init-env "bg-color" (if (== (get-preference "gui theme") "liii-night") "#4a2c2c" "#fff0f0"))))
     (when too-many-matches?
       ;;(display* "Extend limit to " (* 2 limit) "\n")
       (delayed
