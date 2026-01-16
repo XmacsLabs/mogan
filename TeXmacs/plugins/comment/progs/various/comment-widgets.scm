8d644832381a35f12cae8b18ad4512be473d430f
wumo <he17802723@163.com>
2026-01-16 10:39:02 +0800

    修复linux打包版本号错误

--- TeXmacs/plugins/comment/progs/various/comment-widgets.scm
+++ TeXmacs/plugins/comment/progs/various/comment-widgets.scm
@@ -119,9 +119,9 @@
   (:require (inside? 'carbon-comment))
   (comment-editor-done))
 
-(tm-define (make-folded-comment type)
-  (:applicable (not (inside-comment?)))
-  (make-comment 'folded-comment type (list 1) open-comment-editor))
+; (tm-define (make-folded-comment type)
+;   (:applicable (not (inside-comment?)))
+;   (make-comment 'folded-comment type (list 1) open-comment-editor))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Open comments editor
@@ -241,6 +241,100 @@
             (when inv? (tree-select (tm-ref c :last)))
             (tree-go-to c :start))
           (selection-cancel)))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 新增功能：先编辑，确认后再插入 (仿照 open-comment-editor-aux 修正版)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 辅助函数：从编辑器复杂的树结构中提取用户输入的核心 document
+;; 结构通常是: (with ... (document (hide-preamble ...) (document "用户内容")))
+(define (extract-editor-body t)
+  (cond 
+    ;; 如果是 (with ... body)，取最后一个孩子继续剥离
+    ((tm-func? t 'with) 
+     (extract-editor-body (tm-ref t :last)))
+    
+    ;; 如果是 (document ...)，检查是否包含 hide-preamble
+    ((tm-func? t 'document)
+     (if (and (> (tree-arity t) 0) 
+              (tm-func? (tm-ref t 0) 'hide-preamble))
+         ;; 如果第一个孩子是 hide-preamble，说明这是外层包裹，取最后一个孩子（即真正的编辑器内容）
+         (extract-editor-body (tm-ref t :last))
+         ;; 否则这就是最内层的 document，直接返回
+         t))
+    
+    ;; 其他情况直接返回
+    (else t)))
+
+;; 1. 提交函数
+(define (new-comment-done target-buffer target-path type)
+  (let* ((editor-buffer (current-buffer)) ;; 获取当前点击按钮时的 aux buffer
+         (editor-root (buffer-get-body editor-buffer))
+         ;; 【关键】使用提取函数拿到干净的内容，并深拷贝
+         (clean-content (tree-copy (extract-editor-body editor-root))))
+    
+    ;; 关闭窗口
+    (set-comment-window-state #f)
+    (show-auxiliary-widget #f)
+    
+    ;; 切换回主文档插入
+    (if (buffer-exists? target-buffer)
+        (with-buffer target-buffer
+          (tree-go-to target-path)
+          ;; 插入批注，使用 lambda 返回提取出的干净内容
+          (make-comment 'folded-comment 
+                        type 
+                        (list 1) 
+                        (lambda () clean-content)))
+        (display "Error: Target buffer lost.\n"))))
+
+;; 2. Widget 定义 (参数传递版)
+(tm-widget ((new-comment-aux-widget u packs doc target-buf target-path type) quit)
+  (padded
+    (resize "480px" "300px"
+      (texmacs-input doc `(style (tuple ,@packs)) u))
+    ===
+    (hlist
+      >>
+      (explicit-buttons
+        ("Insert" (new-comment-done target-buf target-path type))))))
+
+;; 3. 打开编辑器 (完全仿照 open-comment-editor-aux 的环境构建)
+(tm-define (open-new-comment-editor-aux target-buf target-path type)
+  (and-let* ((b target-buf) ;; 主文档 buffer
+             (u (string->url "tmfs://aux/new-comment"))
+             (packs (embedded-style-list))
+             ;; 获取主文档的 preamble 和初始化环境，保证显示效果一致
+             (pre (document-get-preamble (buffer-tree)))
+             (inits* (map cdr (cdr (tm->stree (get-all-inits)))))
+             (inits (list-filter inits* allow-init?))
+             (env (apply append inits))
+             ;; 【核心差异】
+             ;; 原版这里是 (mirror-comment ...)，我们这里放一个空的 (document "")
+             ;; 这样结构就保持了一致：(with ... (document (hide-preamble ...) (document "")))
+             (empty-body '(document ""))
+             (doc `(with ,@env (document (hide-preamble ,pre) ,empty-body))))
+    
+    ;; 关联 Master，虽然我们在 done 里显式传了 buffer，但这一步对样式渲染很重要
+    (buffer-set-master u b)
+    
+    ;; 启动 Widget，传入所有必要参数
+    (auxiliary-widget (new-comment-aux-widget u packs doc target-buf target-path type)
+                      (comment-cancel b)
+                      "New Comment" u)
+    
+    (buffer-focus "tmfs://aux/new-comment" #t)))
+
+;; 4. 入口函数
+(tm-define (make-folded-comment type)
+  (:applicable (not (inside-comment?)))
+  (:interactive #t)
+  
+  (let ((current-buf (current-buffer))
+        (current-path (cursor-path)))
+    (set-comment-window-state #t)
+    (open-new-comment-editor-aux current-buf current-path type)))
 
 (tm-define (mouse-event key x y mods time data)
   (former key x y mods time data)
